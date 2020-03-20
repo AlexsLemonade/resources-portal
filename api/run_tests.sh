de30585bc8975c9e64779f6be0c17064d894c4bc
Kurt Wheeler <kurt.wheeler91@gmail.com>
2020-03-20 10:45:18 -0400

    Actually run coverage to generate a .coverage file.

--- api/run_tests.sh
+++ api/run_tests.sh
@@ -1,14 +1,5 @@
 #!/bin/bash -e
 
 python wait_for_postgres.py
-echo "#############################"
-printenv
-echo "#############################"
-ls /tmp
-echo "#############################"
-./manage.py test
-echo "#############################"
-ls -lah resources_portal
-echo "#############################"
-ls -lah
+coverage run ./manage.py test --no-input
 coverage report -m
