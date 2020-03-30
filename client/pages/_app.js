c6f4468cba67817c2f0143dfbcbbe8bda7ad01eb
David Mejia <davidsmejia@gmail.com>
2020-03-30 15:07:45 -0400

    adding grommet

---
+++ client/pages/_app.js
@@ -0,0 +1,23 @@
+import { Grommet } from 'grommet';
+import theme from '../src/theme';
+import Head from 'next/head';
+
+// global styles
+import '../src/styles/app.scss'
+
+export default ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Arvo:400,700|Lato:400,400i,700&display=swap"
+          rel="stylesheet"
+          key="google-fonts"
+        />
+      </Head>
+      <Grommet theme={theme}>
+        <Component {...pageProps} />
+      </Grommet>
+    </>
+  )
+}
