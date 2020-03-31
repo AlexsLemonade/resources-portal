cf7d33f16eb9c8bbecf9cb4f1222967a41243413
Ariel <arielsvn@gmail.com>
2020-03-30 20:23:44 -0400

    setup styled components

---
+++ client/src/components/Layout.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Header from './Header';
+import styled from 'styled-components';
+
+export default function Layout({ children }) {
+  return (
+    <LayoutContainer>
+      <Header />
+
+      <LayoutContent>{children}</LayoutContent>
+    </LayoutContainer>
+  );
+}
+
+const LayoutContainer = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const LayoutContent = styled.div`
+  max-width: 1175px;
+  padding: 0;
+  width: 100%;
+  flex: 1;
+`;
