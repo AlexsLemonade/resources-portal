ad0d11fd9355d70caa648450a6541590b6664077
Ariel <arielsvn@gmail.com>
2020-03-31 23:05:47 -0400

    add buttons default styles

---
+++ client/src/components/base.stories.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { Grommet, Button, Box } from 'grommet';
+import { storiesOf } from '@storybook/react';
+
+import theme from '../theme';
+
+storiesOf('Button', module).add('default', () => {
+  return (
+    <Grommet theme={theme}>
+      <Box align="center" pad="large" gap="large">
+        <Button label="Primary" primary />
+
+        <Button label="Secondary" />
+      </Box>
+    </Grommet>
+  );
+});
