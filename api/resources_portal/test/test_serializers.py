34ebc31121eee5b36710d0a3a96e3cd0836a593c
Kurt Wheeler <kurt.wheeler91@gmail.com>
2020-04-01 10:19:02 -0400

    Switch from nose asserts to unittest asserts.

--- api/resources_portal/test/test_serializers.py
+++ api/resources_portal/test/test_serializers.py
@@ -2,8 +2,6 @@ from django.contrib.auth.hashers import check_password
 from django.forms.models import model_to_dict
 from django.test import TestCase
 
-from nose.tools import eq_, ok_
-
 from resources_portal.test.factories import UserFactory
 from resources_portal.views.user import CreateUserSerializer
 
@@ -14,15 +12,15 @@ class TestCreateUserSerializer(TestCase):
 
     def test_serializer_with_empty_data(self):
         serializer = CreateUserSerializer(data={})
-        eq_(serializer.is_valid(), False)
+        self.assertEqual(serializer.is_valid(), False)
 
     def test_serializer_with_valid_data(self):
         serializer = CreateUserSerializer(data=self.user_data)
-        ok_(serializer.is_valid())
+        self.assertTrue(serializer.is_valid())
 
     def test_serializer_hashes_password(self):
         serializer = CreateUserSerializer(data=self.user_data)
-        ok_(serializer.is_valid())
+        self.assertTrue(serializer.is_valid())
 
         user = serializer.save()
-        ok_(check_password(self.user_data.get("password"), user.password))
+        self.assertTrue(check_password(self.user_data.get("password"), user.password))
