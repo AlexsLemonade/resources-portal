ba2932fe1e47e7c1727654a4ed5f40bdf4f6a31b
Ariel <arielsvn@gmail.com>
2020-04-04 01:05:06 -0400

    add search page

---
+++ client/src/pages/search.js
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+
+const Home = () => (
+  <div className="container">
+    <main>
+      <h1 className="title">Search page</h1>
+    </main>
+  </div>
+);
+
+export default Home;
