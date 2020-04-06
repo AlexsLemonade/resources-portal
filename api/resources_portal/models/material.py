6e12735ef7fdd0384868d823220961cb68a6284b
Philip Jones <philipjones@CCDLs-MacBook-Pro.local>
2020-04-06 15:01:35 -0400

    Adding script to insert JSON test data into postgresql database

--- api/resources_portal/models/material.py
+++ api/resources_portal/models/material.py
@@ -16,6 +16,8 @@ class Material(models.Model):
         ("DATASET", "DATASET"),
         ("MOUSE_MODEL", "MOUSE_MODEL"),
         ("ZEBRAFISH_MODEL", "ZEBRAFISH_MODEL"),
+        ("PDX", "PDX"),
+        ("OTHER", "OTHER"),
     )
 
     objects = models.Manager()
@@ -31,6 +33,11 @@ class Material(models.Model):
     mta_s3_url = models.TextField(
         blank=True, null=True, help_text="Contains an url to download the MTA."
     )
+
+    title = models.TextField(
+        blank=False, null=False, help_text="The title of the material.", default="New Material"
+    )
+
     needs_mta = models.BooleanField(default=False)
     needs_irb = models.BooleanField(default=False)
 
