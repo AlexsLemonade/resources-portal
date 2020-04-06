225cdc313bc6398043dc134f927c6fcad9d309d1
Ariel <arielsvn@gmail.com>
2020-04-06 16:43:58 -0400

    extend from prettier rules

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,19 +1,8 @@
 module.exports = {
+  extends: ['airbnb', 'plugin:prettier/recommended'],
   env: {
     browser: true,
     es6: true,
-  },
-  extends: ['airbnb'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module',
   },
   plugins: ['react'],
   rules: {
