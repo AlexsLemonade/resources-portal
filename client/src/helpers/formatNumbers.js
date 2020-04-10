fa87a2e610053c979693740cae60a3ee499fed0d
David Mejia <davidsmejia@gmail.com>
2020-04-10 17:38:05 -0400

    removing eslint for now

--- client/src/helpers/formatNumbers.js
+++ client/src/helpers/formatNumbers.js
@@ -1,11 +1,10 @@
 export const internationalFormat = (value) => {
-  const num = parseInt(value, 10)
-  return new Intl.NumberFormat().format(Math.round(num * 10) / 10)
+  return new Intl.NumberFormat().format(Math.round(value * 10) / 10)
 }
 
 export const abbreviateNumbers = (value) => {
   const num = parseInt(value, 10)
-  if (num >= 1000000) {
+  if (num >= 1000 ** 2) {
     return `${internationalFormat(num / 1000000)}M`
   }
   if (num >= 1000) {
