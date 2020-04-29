6511cd78434eaadd171f39150c671db72b604e70
Kurt Wheeler <kurt.wheeler91@gmail.com>
2020-04-29 17:48:17 -0400

    Remove unused imports.

--- api/resources_portal/models/attachment.py
+++ api/resources_portal/models/attachment.py
@@ -1,8 +1,4 @@
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-from resources_portal.models.user import User
 
 
 class Attachment(models.Model):
