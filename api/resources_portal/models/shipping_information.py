b75d4241bc698a6f2d9851471cdd3bfc587f26d7
Kurt Wheeler <kurt.wheeler91@gmail.com>
2020-04-29 18:03:38 -0400

    Initial proposal for ShippingInformation model.

---
+++ api/resources_portal/models/shipping_information.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class ShippingInformation(models.Model):
+    class Meta:
+        db_table = "shipping_information"
+        get_latest_by = "created_at"
+
+    objects = models.Manager()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    shipping_address = models.BooleanField(default=False)
+    sharer_pays = models.BooleanField(default=False)
+
+    PAYMENT_CHOICES = (
+        ("UPS_OR_FEDEX_CODE", "UPS_OR_FEDEX_CODE"),
+        ("REIMBURSEMENT", "REIMBURSEMENT"),
+        ("OTHER", "OTHER"),
+    )
+    payment_method = models.CharField(max_length=32, choices=PAYMENT_CHOICES)
+
+    restrictions = models.TextField(blank=False, null=True)
+
+    deleted = models.BooleanField(default=False)
