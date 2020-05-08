d2b8a45974b22e572a3eea724d583c75802ade0b
David Mejia <davidsmejia@gmail.com>
2020-05-08 14:15:39 -0400

    remove whitespace

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -5,7 +5,6 @@ const Home = () => (
   <div className="container">
     <main>
       <h1 className="title">TODO: Landing page goes here</h1>
-
       <h3>Api client at: {path}</h3>
     </main>
   </div>
