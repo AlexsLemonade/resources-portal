e69139dc7ee43315f5a634833b96f3409f9ca132
Philip Jones <philipjones@CCDLs-MacBook-Pro.local>
2020-05-22 11:39:14 -0400

    modified __init__

--- api/resources_portal/views/__init__.py
+++ api/resources_portal/views/__init__.py
@@ -4,7 +4,6 @@ from resources_portal.views.document_views import (
     UserDocumentView,
 )
 from resources_portal.views.grant import GrantViewSet
-from resources_portal.views.grant_material_association import GrantMaterialViewSet
 from resources_portal.views.material import MaterialViewSet
 from resources_portal.views.organization import OrganizationViewSet
 from resources_portal.views.organization_invitations import OrganizationInvitationViewSet
