05b2eb4709c339624cd7304e3827c54da4b23c17
Philip Jones <philipjones@CCDLs-MacBook-Pro.local>
2020-06-03 13:51:01 -0400

    Added user soft-delete

--- api/resources_portal/models/user.py
+++ api/resources_portal/models/user.py
@@ -1,18 +1,32 @@
 import uuid
 
 from django.conf import settings
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 
 
+class NonDeletedObjectsManager(UserManager):
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted=False)
+
+
+class DeletedObjectsManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted=True)
+
+
 class User(AbstractUser):
+    objects = NonDeletedObjectsManager()
+    deleted_objects = DeletedObjectsManager()
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     published_name = models.TextField()
+    deleted = models.BooleanField(default=False, null=False)
 
     organizations = models.ManyToManyField("Organization", through="OrganizationUserAssociation")
 
