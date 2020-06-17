851da3402a9ef4530d6d5c454a28a626ba1c07d3
Kurt Wheeler <kurt.wheeler91@gmail.com>
2020-06-17 15:54:41 -0400

    Holy smokes it works!

--- infrastructure/deploy.py
+++ infrastructure/deploy.py
@@ -6,141 +6,154 @@ https://github.com/AlexsLemonade/resources-portal/issues/33
 
 import argparse
 import os
-import shutil
 import signal
 import subprocess
 
+from init_terraform import init_terraform
+
 # import docker
 
-description = """This script can be used to deploy and update a `resources portal` instance stack.
-It will create all of the AWS infrasctructure (roles/instances/db/network/etc),
-open an ingress, perform a database migration, and close the
-ingress. This can be run from a CI/CD machine or a local dev box.
-This script must be run from /infrastructure!"""
-parser = argparse.ArgumentParser(description=description)
 
-env_help_text = """Specify the environment you would like to deploy to. Not optional. Valid values
-are: prod, staging, and dev `prod` and `staging` will deploy the production stack. These should
-only be used from a deployment machine. `dev` will deploy a dev stack which is appropriate for a
-single developer to use to test."""
-parser.add_argument(
-    "-e", "--env", help=env_help_text, required=True, choices=["dev", "staging", "prod"],
-)
+def parse_args():
+    description = """This script can be used to deploy and update a `resources portal` instance stack.
+    It will create all of the AWS infrasctructure (roles/instances/db/network/etc),
+    open an ingress, perform a database migration, and close the
+    ingress. This can be run from a CI/CD machine or a local dev box.
+    This script must be run from /infrastructure!"""
+    parser = argparse.ArgumentParser(description=description)
 
-user_help_text = (
-    "Specify the username of the deployer. Should be the developer's name in development stacks."
-)
-parser.add_argument("-u", "--user", help=user_help_text, required=True)
+    env_help_text = """Specify the environment you would like to deploy to. Not optional. Valid values
+    are: prod, staging, and dev `prod` and `staging` will deploy the production stack. These should
+    only be used from a deployment machine. `dev` will deploy a dev stack which is appropriate for a
+    single developer to use to test."""
+    parser.add_argument(
+        "-e", "--env", help=env_help_text, required=True, choices=["dev", "staging", "prod"],
+    )
 
-dockerhub_help_text = (
-    "Specify the dockerhub repo from which to pull the docker image."
-    " Can be useful for using your own dockerhub repo for a development stack."
-)
-parser.add_argument("-d", "--dockerhub-repo", help=dockerhub_help_text, required=True)
+    user_help_text = "Specify the username of the deployer. Should be the developer's name in development stacks."
+    parser.add_argument("-u", "--user", help=user_help_text, required=True)
 
-version_help_text = "Specify the version of the system that is being deployed."
-parser.add_argument("-v", "--system-version", help=version_help_text, required=True)
+    dockerhub_help_text = (
+        "Specify the dockerhub repo from which to pull the docker image."
+        " Can be useful for using your own dockerhub repo for a development stack."
+    )
+    parser.add_argument("-d", "--dockerhub-repo", help=dockerhub_help_text, required=True)
 
-region_help_text = "Specify the AWS region to deploy the stack to. Default is us-east-1."
-parser.add_argument("-r", "--region", help=region_help_text, default="us-east-1")
+    version_help_text = "Specify the version of the system that is being deployed."
+    parser.add_argument("-v", "--system-version", help=version_help_text, required=True)
 
-args = parser.parse_args()
+    region_help_text = "Specify the AWS region to deploy the stack to. Default is us-east-1."
+    parser.add_argument("-r", "--region", help=region_help_text, default="us-east-1")
 
-# This could be configurable, but there isn't much point.
-HTTP_PORT = 8081
-
-image_name = f"{args.dockerhub_repo}/resources_portal_api:{args.system_version}"
-
-# Hopefully this gets answered and we can use docker-py:
-# https://github.com/docker/docker-py/issues/2526
-#  client = docker.DockerClient(base_url="unix://var/run/docker.sock", version="auto")
-# # client = docker.from_env(version="auto")
-# client.images.build(
-#     path="../api",
-#     dockerfile="Dockerfile.prod",
-#     tag=image_name,
-#     buildargs={"SYSTEM_VERSION": args.system_version, "HTTP_PORT": HTTP_PORT},
-# )
-
-# Until then, use subprocess to call docker :(
-# Change dir so docker can see the code.
-os.chdir("../api")
-
-system_version_build_arg = "SYSTEM_VERSION={}".format(args.system_version)
-http_port_build_arg = "HTTP_PORT={}".format(HTTP_PORT)
-
-# check_call() will raise an exception for us if this fails.
-completed_command = subprocess.check_call(
-    [
-        "docker",
-        "build",
-        "--tag",
-        image_name,
-        "--build-arg",
-        system_version_build_arg,
-        "--build-arg",
-        http_port_build_arg,
-        "-f",
-        "Dockerfile.prod",
-        ".",
-    ],
-)
-
-completed_command = subprocess.check_call(["docker", "push", image_name])
-
-# Change dir back so terraform is run from the correct location:
-os.chdir("../infrastructure")
-
-# Load environment specific variables.
-if args.env == "dev":
-    with open("api-configuration/dev-secrets") as dev_secrets:
-        for line in dev_secrets.readlines():
-            [key, val] = line.split("=")
-            # Test this!
-            os.environ[key] = val
-else:
-    env_prefix = args.env.upper() + "_"
-    for key in filter(lambda var: var.startswith(env_prefix), os.environ.keys()):
-        val = os.environ.get(key)
-        stripped_key = key.split(env_prefix)[-1]
-        os.environ[stripped_key] = val
+    return parser.parse_args()
 
 
-os.environ["TF_VAR_user"] = args.user
-os.environ["TF_VAR_stage"] = args.env
-os.environ["TF_VAR_region"] = args.region
-os.environ["TF_VAR_dockerhub_repo"] = args.dockerhub_repo
-os.environ["TF_VAR_system_version"] = args.system_version
+def build_and_push_docker_image(args):
+    # This could be configurable, but there isn't much point.
+    HTTP_PORT = 8081
 
-var_file_arg = "-var-file=tf_vars/{}.tfvars".format(args.env)
+    image_name = f"{args.dockerhub_repo}/resources_portal_api:{args.system_version}"
 
-init_bucket = f'-backend-config="bucket=resources-portal-tfstate-{args.env}"'
-init_key = f'-backend-config="key=terraform-{args.user}.tfstate"'
+    # Hopefully this gets answered and we can use docker-py:
+    # https://github.com/docker/docker-py/issues/2526
+    #  client = docker.DockerClient(base_url="unix://var/run/docker.sock", version="auto")
+    # # client = docker.from_env(version="auto")
+    # client.images.build(
+    #     path="../api",
+    #     dockerfile="Dockerfile.prod",
+    #     tag=image_name,
+    #     buildargs={"SYSTEM_VERSION": args.system_version, "HTTP_PORT": HTTP_PORT},
+    # )
 
-# Make sure that Terraform is allowed to shut down gracefully.
-try:
-    command = [
-        "terraform",
-        "init",
-        init_bucket,
-        init_key,
-        '-backend-config="dynamodb_table=resources-portal-terraform-lock"',
-        var_file_arg,
-        "-force-copy",
-    ]
-    print(command)
-    terraform_process = subprocess.Popen(command)
-    terraform_process.wait()
-except KeyboardInterrupt:
-    terraform_process.send_signal(signal.SIGINT)
-    terraform_process.wait()
+    # Until then, use subprocess to call docker :(
+    # Change dir so docker can see the code.
+    os.chdir("../api")
 
-exit()
+    system_version_build_arg = "SYSTEM_VERSION={}".format(args.system_version)
+    http_port_build_arg = "HTTP_PORT={}".format(HTTP_PORT)
 
-try:
-    terraform_process = subprocess.Popen(["terraform", "apply", var_file_arg, "-auto-approve"])
-    terraform_process.wait()
-    exit(terraform_process.returncode)
-except KeyboardInterrupt:
-    terraform_process.send_signal(signal.SIGINT)
-    terraform_process.wait()
+    # check_call() will raise an exception for us if this fails.
+    completed_command = subprocess.check_call(
+        [
+            "docker",
+            "build",
+            "--tag",
+            image_name,
+            "--build-arg",
+            system_version_build_arg,
+            "--build-arg",
+            http_port_build_arg,
+            "-f",
+            "Dockerfile.prod",
+            ".",
+        ],
+    )
+
+    completed_command = subprocess.check_call(["docker", "push", image_name])
+
+    # Change dir back so terraform is run from the correct location:
+    os.chdir("../infrastructure")
+
+    return completed_command
+
+
+def load_env_vars(args):
+    """Load environment specific variables.
+
+    For dev environment, just use the variables contained in
+    api-configuration/dev-secrets.
+
+    For staging and prod environments, filter all the environment
+    variables just starting with "STAGING_" or "PROD_". This is
+    because github actions will always provide all the env variables.
+    """
+    if args.env == "dev":
+        with open("api-configuration/dev-secrets") as dev_secrets:
+            for line in dev_secrets.readlines():
+                [key, val] = line.split("=")
+                # Test this!
+                os.environ[key] = val
+    else:
+        env_prefix = args.env.upper() + "_"
+        for key in filter(lambda var: var.startswith(env_prefix), os.environ.keys()):
+            val = os.environ.get(key)
+            stripped_key = key.split(env_prefix)[-1]
+            os.environ[stripped_key] = val
+
+    os.environ["TF_VAR_user"] = args.user
+    os.environ["TF_VAR_stage"] = args.env
+    os.environ["TF_VAR_region"] = args.region
+    os.environ["TF_VAR_dockerhub_repo"] = args.dockerhub_repo
+    os.environ["TF_VAR_system_version"] = args.system_version
+
+
+def run_terraform(args):
+    var_file_arg = "-var-file=tf_vars/{}.tfvars".format(args.env)
+
+    try:
+        terraform_process = subprocess.Popen(["terraform", "apply", var_file_arg, "-auto-approve"])
+        terraform_process.wait()
+        return terraform_process.returncode
+    except KeyboardInterrupt:
+        terraform_process.send_signal(signal.SIGINT)
+        terraform_process.wait()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    docker_code = build_and_push_docker_image(args)
+
+    if docker_code != 0:
+        exit(docker_code)
+
+    load_env_vars(args)
+
+    init_code = init_terraform(args.env, args.user)
+
+    if init_code != 0:
+        exit(init_code)
+
+    # This is the last command, so the script's return code should
+    # match it.
+    exit(run_terraform(args))
