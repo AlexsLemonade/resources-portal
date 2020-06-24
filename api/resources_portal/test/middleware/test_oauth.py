e1bd93dac69a744edb7e9613e703a745d9adc9e2
Philip Jones <philipjones@CCDLs-MacBook-Pro.local>
2020-06-24 13:21:18 -0400

    renamed ORCID variables

--- api/resources_portal/test/middleware/test_oauth.py
+++ api/resources_portal/test/middleware/test_oauth.py
@@ -6,11 +6,11 @@ from rest_framework.test import APITestCase
 from resources_portal.models import User
 from resources_portal.test.factories import GrantFactory
 from resources_portal.test.mocks import (
-    AUTHORIZATION_DICT,
     MOCK_EMAIL,
-    generate_mock_authorization_response,
+    ORCID_AUTHORIZATION_DICT,
+    generate_mock_orcid_authorization_response,
     generate_mock_orcid_record_response,
-    get_mock_url,
+    get_mock_oauth_url,
 )
 
 
@@ -23,16 +23,16 @@ class TestOAuthUserCreation(APITestCase):
         self.grant1 = GrantFactory()
         self.grant2 = GrantFactory()
 
-        self.url = get_mock_url([self.grant1, self.grant2])
+        self.url = get_mock_oauth_url([self.grant1, self.grant2])
 
     @patch("orcid.PublicAPI", side_effect=generate_mock_orcid_record_response)
-    @patch("requests.post", side_effect=generate_mock_authorization_response)
+    @patch("requests.post", side_effect=generate_mock_orcid_authorization_response)
     def test_oauth_flow_creates_new_user(self, mock_auth_request, mock_record_request):
         response = self.client.get(self.url)
 
         user = User.objects.get(pk=self.client.session["_auth_user_id"])
 
-        self.assertEqual(user.orcid, AUTHORIZATION_DICT["orcid"])
+        self.assertEqual(user.orcid, ORCID_AUTHORIZATION_DICT["orcid"])
 
         self.assertEqual(user.email, MOCK_EMAIL)
 
--- api/resources_portal/test/mocks.py
+++ api/resources_portal/test/mocks.py
@@ -12,17 +12,19 @@ MOCK_EMAIL = fake.email()
 first_name = fake.first_name()
 last_name = fake.last_name()
 
-AUTHORIZATION_DICT = {
+ORCID_AUTHORIZATION_DICT = {
     "name": f"{first_name} {last_name}",
     "orcid": str(uuid.uuid4()),
     "access_token": str(uuid.uuid4()),
     "refresh_token": str(uuid.uuid4()),
 }
 
-SUMMARY_DICT = {"name": {"given-names": {"value": first_name}, "family-name": {"value": last_name}}}
+ORCID_SUMMARY_DICT = {
+    "name": {"given-names": {"value": first_name}, "family-name": {"value": last_name}}
+}
 
 
-class MockAuthorizationResponse:
+class MockORCIDAuthorizationResponse:
     def __init__(self, json_data, status_code):
         self.json_data = json_data
         self.status_code = status_code
@@ -31,7 +33,7 @@ class MockAuthorizationResponse:
         return self.json_data
 
 
-class MockRecordResponse:
+class MockORCIDRecordResponse:
     def __init__(self, summary):
         self.summary = summary
 
@@ -40,15 +42,15 @@ class MockRecordResponse:
 
 
 # This method will be used by the mock to replace requests.get
-def generate_mock_authorization_response(*args, **kwargs):
-    return MockAuthorizationResponse(AUTHORIZATION_DICT, 200)
+def generate_mock_orcid_authorization_response(*args, **kwargs):
+    return MockORCIDAuthorizationResponse(ORCID_AUTHORIZATION_DICT, 200)
 
 
 def generate_mock_orcid_record_response(*args, **kwargs):
-    return MockRecordResponse(SUMMARY_DICT)
+    return MockORCIDRecordResponse(ORCID_SUMMARY_DICT)
 
 
-def get_mock_url(grants):
+def get_mock_oauth_url(grants):
     base_url = reverse("user-list")
     url = f"{base_url}" f"?code={MOCK_AUTHORIZATION_CODE}" f"&email={MOCK_EMAIL}"
 
