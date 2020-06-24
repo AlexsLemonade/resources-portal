fbc5ec8dc394b41d93263e16ed26b1ff6c5d37b9
Philip Jones <philipjones@CCDLs-MacBook-Pro.local>
2020-06-24 13:45:39 -0400

    basic test structure added

--- api/resources_portal/test/end_to_end/test_user_creates_account.py
+++ api/resources_portal/test/end_to_end/test_user_creates_account.py
@@ -5,14 +5,14 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from resources_portal.models import User
+from resources_portal.models import Material, User
 from resources_portal.test.factories import GrantFactory, MaterialFactory
 from resources_portal.test.mocks import (
-    AUTHORIZATION_DICT,
     MOCK_EMAIL,
-    generate_mock_authorization_response,
+    ORCID_AUTHORIZATION_DICT,
+    generate_mock_orcid_authorization_response,
     generate_mock_orcid_record_response,
-    get_mock_url,
+    get_mock_oauth_url,
 )
 
 
@@ -30,24 +30,30 @@ class TestUserCreatesAccount(APITestCase):
         self.grant2 = GrantFactory()
 
     @patch("orcid.PublicAPI", side_effect=generate_mock_orcid_record_response)
-    @patch("requests.post", side_effect=generate_mock_authorization_response)
+    @patch("requests.post", side_effect=generate_mock_orcid_authorization_response)
     def test_oauth_flow_creates_new_user(self, mock_auth_request, mock_record_request):
+        # Create user with ORCID
+        self.client.get(get_mock_oauth_url([self.grant1, self.grant2]))
 
-        response = self.client.get(get_mock_url([self.grant1, self.grant2]))
-
+        # Client is now logged in as user, get user ID from session data
         user = User.objects.get(pk=self.client.session["_auth_user_id"])
 
-        material = MaterialFactory(contact_user=user, organization=self.user.organizations.first())
+        import pdb
+
+        pdb.set_trace()
+
+        # Create resource on personal organization
+        material = MaterialFactory(contact_user=user, organization=user.organizations.first())
         material_data = model_to_dict(material)
 
         response = self.client.post(reverse("material-list"), material_data, format="json")
+
+        # Resource assertions
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(material.contact_user, response.data["contact_user"])
+        self.assertEqual(material.organization, response.data["organization"])
 
-        self.assertEqual(user.orcid, AUTHORIZATION_DICT["orcid"])
-
+        # User assertions
         self.assertEqual(user.email, MOCK_EMAIL)
-
         self.assertTrue(self.grant1 in user.grants.all())
         self.assertTrue(self.grant2 in user.grants.all())
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
