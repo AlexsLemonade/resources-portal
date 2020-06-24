393f70824e194b716973bdc8e87fa3b6a8a48495
Philip Jones <philipjones@CCDLs-MacBook-Pro.local>
2020-06-24 15:15:29 -0400

    added test for logging in existing users

--- api/resources_portal/config/production.py
+++ api/resources_portal/config/production.py
@@ -12,7 +12,7 @@ class Production(Common):
     INSTALLED_APPS += ("gunicorn",)
 
     # OAuth
-    OAUTH_URL = "https://orcid.org/oauth/token"
+    OAUTH_URL = os.getenv("OAUTH_URL")
 
     # https://developers.google.com/web/fundamentals/performance/optimizing-content-efficiency/http-caching#cache-control
     # Response can be cached by browser and any intermediary caches (i.e. it is "public") for up to 1 day
