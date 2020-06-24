d5e756bff01c515db951ad6f6547690ee2511838
Philip Jones <philipjones@CCDLs-MacBook-Pro.local>
2020-06-24 13:16:42 -0400

    added test infastructure

--- api/resources_portal/test/end_to_end/test_user_creates_account.py
+++ api/resources_portal/test/end_to_end/test_user_creates_account.py
@@ -1,77 +1,47 @@
 from unittest.mock import patch
 
+from django.forms.models import model_to_dict
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
 from resources_portal.models import User
-from resources_portal.test.factories import GrantFactory
-
-MOCK_AUTHORIZATION_CODE = "mock"
-MOCK_EMAIL = "test@mock.com"
-
-AUTHORIZATION_DICT = {
-    "name": "Elong Musket",
-    "orcid": "1234-Iwan-naGo-t0maRs",
-    "access_token": "CyBe-RTru-KIn-SpAce",
-    "refresh_token": "Pl3A-SeBuY-mYSt0-kS",
-}
-
-SUMMARY_DICT = {"name": {"given-names": {"value": "Elong"}, "family-name": {"value": "Musket"}}}
+from resources_portal.test.factories import GrantFactory, MaterialFactory
+from resources_portal.test.mocks import (
+    AUTHORIZATION_DICT,
+    MOCK_EMAIL,
+    generate_mock_authorization_response,
+    generate_mock_orcid_record_response,
+    get_mock_url,
+)
 
 
-class MockAuthorizationResponse:
-    def __init__(self, json_data, status_code):
-        self.json_data = json_data
-        self.status_code = status_code
-
-    def json(self):
-        return self.json_data
-
-
-class MockRecordResponse:
-    def __init__(self, summary):
-        self.summary = summary
-
-    def read_record_public(self, client_id, client_secret, sandbox):
-        return self.summary
-
-
-# This method will be used by the mock to replace requests.get
-def generate_mock_authorization_response(*args, **kwargs):
-    return MockAuthorizationResponse(AUTHORIZATION_DICT, 200)
-
-
-def generate_mock_orcid_record_response(*args, **kwargs):
-    return MockRecordResponse(SUMMARY_DICT)
-
-
-class TestOAuthUserCreation(APITestCase):
+class TestUserCreatesAccount(APITestCase):
     """
-    Tests OAuth's ability to create users.
+    Tests user account creation and resource generation.
+    The flow of the test is as follows:
+
+    1. Create account and supply grant id to be used with personal organization.
+    2. List resource on personal organization.
     """
 
     def setUp(self):
         self.grant1 = GrantFactory()
         self.grant2 = GrantFactory()
 
-        self.grant_ids = [self.grant1.id, self.grant2.id]
-
-        base_url = reverse("user-list")
-        self.url = (
-            f"{base_url}"
-            f"?code={MOCK_AUTHORIZATION_CODE}"
-            f"&email={MOCK_EMAIL}"
-            f"&grant_id={self.grant_ids[0]}"
-            f"&grant_id={self.grant_ids[1]}"
-        )
-
     @patch("orcid.PublicAPI", side_effect=generate_mock_orcid_record_response)
     @patch("requests.post", side_effect=generate_mock_authorization_response)
     def test_oauth_flow_creates_new_user(self, mock_auth_request, mock_record_request):
-        response = self.client.get(self.url)
+
+        response = self.client.get(get_mock_url([self.grant1, self.grant2]))
 
         user = User.objects.get(pk=self.client.session["_auth_user_id"])
+
+        material = MaterialFactory(contact_user=user, organization=self.user.organizations.first())
+        material_data = model_to_dict(material)
+
+        response = self.client.post(reverse("material-list"), material_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         self.assertEqual(user.orcid, AUTHORIZATION_DICT["orcid"])
 
