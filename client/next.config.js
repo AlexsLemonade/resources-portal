1c5c8e0c5c0a91741f2bbb21de51df18667447b6
David Mejia <davidsmejia@gmail.com>
2020-07-31 14:42:04 -0400

    cant use boolean env vars

--- client/next.config.js
+++ client/next.config.js
@@ -2,12 +2,13 @@ const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
 module.exports = (phase) => {
   const isDevelopment = phase === PHASE_DEVELOPMENT_SERVER
+  const isProduction = process.env.STAGE === 'production'
 
   const env = {
     API_VERSION: 'v1',
     IS_DEVELOPMENT: isDevelopment,
-    IS_STAGING: !isDevelopment && !process.env.IS_PRODUCTION,
-    IS_PRODUCTION: !isDevelopment && process.env.IS_PRODUCTION,
+    IS_STAGING: !isDevelopment && !isProduction,
+    IS_PRODUCTION: !isDevelopment && isProduction,
     API_HOST: (() => {
       if (isDevelopment) return 'http://localhost:8000'
       if (process.env.API_HOST) return process.env.API_HOST
