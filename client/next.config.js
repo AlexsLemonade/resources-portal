645b135dac9b0ad66d4d3df91cc99b3e54d8da17
David Mejia <davidsmejia@gmail.com>
2020-07-31 14:32:04 -0400

    update logic for stage envs

--- client/next.config.js
+++ client/next.config.js
@@ -1,25 +1,17 @@
-const {
-  PHASE_DEVELOPMENT_SERVER,
-  PHASE_PRODUCTION_BUILD
-} = require('next/constants')
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
 module.exports = (phase) => {
-  const stageEnv = process.env.STAGING === '1'
   const isDevelopment = phase === PHASE_DEVELOPMENT_SERVER
-  const isStaging = phase === PHASE_PRODUCTION_BUILD && stageEnv
-  const isProduction = phase === PHASE_PRODUCTION_BUILD && !stageEnv
 
   const env = {
     API_VERSION: 'v1',
     IS_DEVELOPMENT: isDevelopment,
-    IS_STAGING: isStaging,
-    IS_PRODUCTION: isProduction,
+    IS_STAGING: !isDevelopment && !process.env.IS_PRODUCTION,
+    IS_PRODUCTION: !isDevelopment && process.env.IS_PRODUCTION,
     API_HOST: (() => {
-      if (process.env.API_HOST) return process.env.API_HOST
       if (isDevelopment) return 'http://localhost:8000'
-      if (isStaging) return 'http://api.staging.resources.alexslemonade.org'
-      if (isProduction) return 'http://api.resources.alexslemonade.org'
-      return 'http://api.resources.alexslemonade.org'
+      if (process.env.API_HOST) return process.env.API_HOST
+      return 'https://api.resources.alexslemonade.org'
     })()
   }
 
