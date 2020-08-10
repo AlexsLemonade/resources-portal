90adca566fdfce77ba8f71d89c3e877cf8820520
Philip Jones <philipjones@ccdls-mbp.myfiosgateway.com>
2020-08-10 17:28:12 -0400

    running into issues with the redirect uri

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,40 +1,45 @@
 import qs from 'qs'
-import React from 'react'
+import React, { Component } from 'react'
 import api, { path } from '../api'
 import StoreToken from '../components/StoreToken'
 
-const Home = ({ token }) => (
-  <div className="container">
-    <main>
-      <h1 className="title">TODO: Landing page goes here</h1>
-      <h3>Api client at: {path}</h3>
-      <StoreToken token={token} />
-    </main>
-  </div>
-)
-
-Home.getInitialProps = async ({ query, req, res }) => {
-  // finish login and redirect
-  if (query.code && query.origin_url) {
-    const { email } = qs.parse(this.props.location.search, {
-      ignoreQueryPrefix: true
-    })
-    console.log('email: ', email)
-    const tokenJson = await api.authenticate(
-      query.code,
-      email,
-      query.origin_url
-    )
-    console.log('Token: ', tokenJson.response.token)
-    if (tokenJson.response.token) {
-      if (req && query.origin_url) {
-        res.writeHead(302, { Location: query.origin_url })
-        res.end()
+export default class Home extends Component {
+  static async getInitialProps(query, req, res) {
+    // finish login and redirect
+    if (query.code && query.origin_url) {
+      const { originURL } = qs.parse(query.origin_url)
+      console.log('Here is the origin url: ', originURL)
+      const { email } = qs.parse(originURL, {
+        ignoreQueryPrefix: true
+      })
+      console.log('email: ', email)
+      const tokenJson = await api.authenticate(query.code, email, originURL)
+      console.log('Token: ', tokenJson.response.token)
+      if (tokenJson.response.token) {
+        if (req && query.origin_url) {
+          res.writeHead(302, { Location: originURL })
+          res.end()
+        }
+        localStorage.setItem('token', tokenJson.response.token)
+        return { token: tokenJson.response.token }
       }
-      return { token: tokenJson.response.token }
-    }
-  }
-  return {}
-}
 
-export default Home
+      console.log('token not found, response was ', tokenJson)
+    } else {
+      console.log('No origin url provided')
+    }
+    return {}
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <main>
+          <h1 className="title">TODO: Landing page goes here</h1>
+          <h3>Api client at: {path}</h3>
+          <StoreToken token={this.props.token} />
+        </main>
+      </div>
+    )
+  }
+}
