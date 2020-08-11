461f19b999f5a883578382c5d0db2bc3e804cd75
Kurt Wheeler <kurt.wheeler91@gmail.com>
2020-08-11 10:51:18 -0400

    Fix a few tests and add a migration file.

--- api/resources_portal/test/serializers/test_user.py
+++ api/resources_portal/test/serializers/test_user.py
@@ -1,3 +1,4 @@
+from django.forms.models import model_to_dict
 from django.test import TestCase
 
 from resources_portal.test.factories import PersonalOrganizationFactory, UserFactory
@@ -6,8 +7,13 @@ from resources_portal.views.user import UserSerializer
 
 class TestCreateUserSerializer(TestCase):
     def setUp(self):
+        # personal_organization needs to be created first because
+        # UserFactory can't build without having an id of an org to
+        # reference.
         personal_organization = PersonalOrganizationFactory()
-        self.user_data = UserFactory.build(personal_organization=personal_organization)
+        self.user_data = model_to_dict(
+            UserFactory.build(personal_organization=personal_organization)
+        )
 
     def test_serializer_with_empty_data(self):
         serializer = UserSerializer(data={})
