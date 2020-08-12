3d9b9d2c26fe8760b748e6597c44849e6d1cc96d
Kurt Wheeler <kurt.wheeler91@gmail.com>
2020-08-12 18:30:24 -0400

    This is untested, but I think it should work.

--- api/resources_portal/models/grant.py
+++ api/resources_portal/models/grant.py
@@ -1,7 +1,13 @@
+from copy import copy
+
 from django.db import models
 
 from safedelete.managers import SafeDeleteDeletedManager, SafeDeleteManager
 from safedelete.models import SOFT_DELETE, SafeDeleteModel
+
+from resources_portal.models.associations.grant_organization_association import (
+    GrantOrganizationAssociation,
+)
 
 
 class Grant(SafeDeleteModel):
@@ -25,3 +31,44 @@ class Grant(SafeDeleteModel):
     )
     organizations = models.ManyToManyField("Organization", through="GrantOrganizationAssociation")
     materials = models.ManyToManyField("Material", through="GrantMaterialAssociation")
+
+    def set_on_personal_organiztion(self):
+        if self.user and self.user.personal_organization:
+            GrantOrganizationAssociation.get_or_create(
+                grant=self, organization=self.user.personal_organization
+            )
+
+    def save(self, *args, **kwargs):
+        """Keep the user's personal organization synced up.
+
+        Because we don't want the users to have to manage grant
+        relationships with their organizations, we want their personal
+        organization's grant list to stay in sync with their user's
+        grant list.
+        """
+        original = None
+        if self.id:
+            original = Grant.objects.get(pk=self.id)
+
+        super().save(*args, **kwargs)
+
+        if original:
+            if original.user != self.user:
+                # Grant user is changing, need to keep relationships
+                # with the personal organizations in sync.
+
+                # First remove the old association, if there was one.
+                if original.user and original.user.personal_organization:
+                    old_association = GrantOrganizationAssociation.objects.filter(
+                        grant=self, organization=original.user.personal_organization
+                    ).first()
+                    if old_association:
+                        old_association.delete()
+
+                # Then add the new association, if there's still an
+                # organization to add it to.
+                self.set_on_personal_organiztion()
+        else:
+            # This was just created, make sure the personal
+            # organization of user is linked if they both exist.
+            self.set_on_personal_organiztion()
