5df6ce936efa608820345e2984cbef7a7a373b6a
David Mejia <davidsmejia@gmail.com>
2020-08-13 16:12:42 -0400

    remove unnamed function declaration

--- client/loaders/template-literal-loader.js
+++ client/loaders/template-literal-loader.js
@@ -1,8 +1,7 @@
 // This is a simple loader used with raw-loader for importing
 // markdown to be consumed by grommet/Markdown
 
-// eslint-disable-next-line
-module.exports = function (source) {
+module.exports = (source) => {
   // eslint-disable-next-line
   return eval(`\`${source}\``)
 }
