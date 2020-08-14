8d7235772be3f6fc80ee069624b184d22579e955
Philip Jones <philipjones@ccdls-mbp.myfiosgateway.com>
2020-08-14 15:39:32 -0400

    added users file

--- api/resources_portal/models/user.py
+++ api/resources_portal/models/user.py
@@ -7,9 +7,8 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from django_expiring_token.models import ExpiringToken
-
 from computedfields.models import ComputedFieldsModel, computed
+from django_expiring_token.models import ExpiringToken
 
 
 class NonDeletedObjectsManager(UserManager):
