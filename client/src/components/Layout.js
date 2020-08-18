ee0e690d55661a7f12a59430626ff7c8817ff9c2
David Mejia <davidsmejia@gmail.com>
2020-08-18 16:16:41 -0400

    updating layout

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import { Box, Main } from 'grommet'
 import Header from './Header'
+import { Footer } from './Footer'
 
-export default function Layout({ children }) {
+export const Layout = ({ children }) => {
   return (
     <Box height={{ min: '100vh' }}>
       <Box gridArea="header" margin={{ bottom: 'xlarge' }}>
         <Header />
       </Box>
-      <Main width="xlarge" alignSelf="center" overflow="visible">
+      <Main width="xxlarge" alignSelf="center" overflow="visible">
         {children}
       </Main>
+      <Footer />
     </Box>
   )
 }
+
+export default Layout
