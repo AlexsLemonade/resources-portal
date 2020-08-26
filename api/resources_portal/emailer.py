3308c6eed915a91c4e95f859e513e1f977f3e69e
Kurt Wheeler <kurt.wheeler91@gmail.com>
2020-08-26 14:49:35 -0400

    Try setting an additional header for emails.

--- api/resources_portal/emailer.py
+++ api/resources_portal/emailer.py
@@ -1,3 +1,4 @@
+import os
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -49,6 +50,9 @@ def create_multipart_message(
             image_part = MIMEImage(f.read(), embedded_image["subtype"])
 
         image_part.add_header("Content-ID", embedded_image["content_id"])
+        image_part.add_header(
+            "Content-Disposition", "inline", filename=os.path.basename(embedded_image["file_path"])
+        )
         msg.attach(image_part)
 
     return msg
