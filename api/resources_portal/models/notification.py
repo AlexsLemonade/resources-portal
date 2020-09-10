627a39ceaddc733105f4d4d06e637a856d4ac12c
Kurt Wheeler <kurt.wheeler91@gmail.com>
2020-09-10 15:50:20 -0400

    Remove another unused import.

--- api/resources_portal/models/notification.py
+++ api/resources_portal/models/notification.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models.signals import post_save, pre_save
