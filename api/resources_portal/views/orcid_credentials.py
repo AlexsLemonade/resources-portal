eaa5851dab76bc2d139ddfe8808fa787a87e2180
Philip Jones <philipjones@ccdls-mbp.myfiosgateway.com>
2020-09-11 12:21:08 -0400

    testing redirect url on staging

--- api/resources_portal/views/orcid_credentials.py
+++ api/resources_portal/views/orcid_credentials.py
@@ -59,7 +59,7 @@ class ORCIDCredentialsViewSet(viewsets.ViewSet):
             return JsonResponse({"error": error}, status=500,)
 
         if "orcid" not in response_json:
-            return JsonResponse(response_json, status=400)
+            return JsonResponse({"response": response_json, "redirect_url": origin_url}, status=400)
 
         return JsonResponse(
             {
