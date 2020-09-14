9d5535055669cd1a3ec19ef70378ef5669f06b6f
David Mejia <davidsmejia@gmail.com>
2020-09-14 15:37:44 -0400

    adding new components

--- client/src/components/DropZone.js
+++ client/src/components/DropZone.js
@@ -2,9 +2,15 @@ import React from 'react'
 import { Box, Text, Button } from 'grommet'
 import Dropzone from 'react-dropzone'
 
-export default ({ fileTypes = [], onDrop }) => {
+// We are restricted to 1GB attachments
+export default ({ fileTypes = [], multiple = true, onDrop }) => {
   return (
-    <Dropzone accept={fileTypes.map((ft) => `.${ft}`)} onDrop={onDrop}>
+    <Dropzone
+      multiple={multiple}
+      accept={fileTypes.map((ft) => `.${ft}`)}
+      onDrop={onDrop}
+      maxSize={1000000000}
+    >
       {({ getRootProps, getInputProps, open }) => (
         <Box
           {...getRootProps()} // eslint-disable-line react/jsx-props-no-spreading
@@ -15,20 +21,26 @@ export default ({ fileTypes = [], onDrop }) => {
             vertical: '32px'
           }}
           border={{
+            size: 'small',
             style: 'dashed'
           }}
         >
           {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-          <input {...getInputProps()} />{' '}
+          <input {...getInputProps()} />
           <Button label="Select Files" onClick={open} primary />
-          <Text color="text-weak" margin="large">
+          <Text color="text-weak" margin={{ top: 'small', bottom: 'medium' }}>
             or drag and drop files
           </Text>
+          <Text color="text-weak" margin={{ top: 'small', bottom: 'medium' }}>
+            Max Size Per File: 1GB
+          </Text>
           <Text color="text-weak" weight="bold">
-            {fileTypes.length
+            {fileTypes.length && fileTypes.length === 1
               ? 'Supported file format:'
               : 'Supported file formats:'}
-            {fileTypes.map((ft) => ` ${ft.toUpperCase()}`)}
+            {fileTypes.map(
+              (ft, i) => `${i > 0 ? ',' : ''} ${ft.toUpperCase()}`
+            )}
           </Text>
         </Box>
       )}
--- client/src/components/HeaderRow.js
+++ client/src/components/HeaderRow.js
@@ -4,12 +4,12 @@ import { Box, Text } from 'grommet'
 export const HeaderRow = ({ label }) => {
   return (
     <Box direction="row" align="center" margin={{ vertical: 'medium' }}>
-      <Text margin={{ right: '4px' }} weight="600" color="brand">
+      <Text weight={600} color="brand">
         {label}
       </Text>
       <Box
         background="brand"
-        margin={{ top: '4px' }}
+        margin={{ left: '4px', top: '4px' }}
         flex="grow"
         height="2px"
       />
