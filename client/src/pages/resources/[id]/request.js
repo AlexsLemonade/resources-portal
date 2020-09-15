7e945e4176699e26c2911efcf24ffde472eb0601
David Mejia <davidsmejia@gmail.com>
2020-09-15 11:25:19 -0400

    adds resource pages

--- client/src/pages/resources/[id]/request.js
+++ client/src/pages/resources/[id]/request.js
@@ -1,9 +1,44 @@
 import React from 'react'
+import { Box, Heading, Anchor } from 'grommet'
+import Link from 'next/link'
+import Error from 'pages/404'
+import dynamic from 'next/dynamic'
+import api from 'api'
+import { getReadable } from 'helpers/readableNames'
 
-export default () => (
-  <div className="container">
-    <main>
-      <h1 className="title">TODO: Resource request page</h1>
-    </main>
-  </div>
+const RequestResourceForm = dynamic(
+  () => import('components/resources/RequestResourceForm'),
+  { ssr: false }
 )
+
+const ResourcesRequest = ({ resource }) => {
+  // you cannot request this resource
+  // it might be deleted
+  if (!resource) return <Error statusCode="404" />
+
+  return (
+    <Box align="center" width="full" margin={{ vertical: 'xlarge' }}>
+      <Box width="large">
+        <Heading level={4} serif alignSelf="start">
+          Request{' '}
+          <Link href={`/resources/${resource.id}`}>
+            <Anchor href="#">{resource.title}</Anchor>
+          </Link>{' '}
+          - {getReadable(resource.category)}
+        </Heading>
+        <RequestResourceForm resource={resource} />
+      </Box>
+    </Box>
+  )
+}
+
+ResourcesRequest.getInitialProps = async ({ query: { id } }) => {
+  const resourceRequest = await api.resources.get(id)
+  if (resourceRequest.isOk) {
+    const { response: resource } = resourceRequest
+    return { resource }
+  }
+  return {}
+}
+
+export default ResourcesRequest
--- client/src/pages/resources/import.js
+++ client/src/pages/resources/import.js
@@ -1,5 +1,21 @@
 import React from 'react'
+import dynamic from 'next/dynamic'
+import { Heading } from 'grommet'
+import { ResourceContextProvider } from 'contexts/ResourceContext'
 
-export default function Import() {
-  return <h1>TODO: Import resource here</h1>
+const ImportResource = dynamic(
+  () => import('components/resources/ImportResource'),
+  { ssr: false }
+)
+export const ResourcesImport = () => {
+  return (
+    <ResourceContextProvider localStorageName="import-resource">
+      <Heading level={4} serif>
+        Import a Resource
+      </Heading>
+      <ImportResource />
+    </ResourceContextProvider>
+  )
 }
+
+export default ResourcesImport
