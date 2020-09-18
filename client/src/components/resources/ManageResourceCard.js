dec7c3f8879c180aafa58b9992a6a612e35c3301
David Mejia <davidsmejia@gmail.com>
2020-09-18 10:30:53 -0400

    support archive delete

--- client/src/components/resources/ManageResourceCard.js
+++ client/src/components/resources/ManageResourceCard.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import { Box, DropButton, Text, Anchor } from 'grommet'
+import { Box, Button, DropButton, Text, Anchor } from 'grommet'
 import Link from 'next/link'
 import { useUser } from 'hooks/useUser'
+import { useAlertsQueue } from 'hooks/useAlertsQueue'
 import api from 'api'
 import Icon from 'components/Icon'
+import { Modal } from 'components/Modal'
 import { ResourceCard } from './ResourceCard'
 
 export const ManageOptions = ({
@@ -11,14 +13,16 @@ export const ManageOptions = ({
   options = [],
   direction,
   pad = 'none',
-  openRequests
+  openRequests,
+  onChange
 }) => {
+  const { token } = useUser()
+  const { addAlert } = useAlertsQueue()
+  const [showDelete, setShowDelete] = React.useState(false)
+  const [showArchive, setShowArchive] = React.useState(false)
   const manageLink = `/account/manage-resources/${resource.id}`
   const editLink = `/resources/${resource.id}/edit`
   const viewResourceLink = `resources/${resource.id}`
-
-  const onArchive = () => {}
-  const onDelete = () => {}
 
   return (
     <Box direction={direction} gap="medium" pad={pad}>
@@ -50,20 +54,142 @@ export const ManageOptions = ({
         </Link>
       )}
       {options.includes('archive') && (
-        <Anchor
-          icon={<Icon name="Archive" size="small" />}
-          label="Archive"
-          onClick={onArchive}
-        />
+        <>
+          <Box
+            border={{ side: 'bottom', color: 'border-black', size: 'small' }}
+          >
+            <Text>Archive Resource</Text>
+          </Box>
+          <Anchor
+            icon={<Icon name="Archive" size="small" />}
+            label="Archive"
+            onClick={() => setShowArchive(true)}
+          />
+          <Modal showing={showArchive} setShowing={setShowArchive}>
+            <>
+              {openRequests === 0 && (
+                <Box>
+                  <Box direction="row">
+                    <Icon name="Warning" color="warning" />
+                    <Text>Are you sure you want to archive this resource?</Text>
+                  </Box>
+                  <Box>
+                    <Text>
+                      Researchers will not be able to request for this resource
+                      but they will still be able to review it.
+                    </Text>
+                  </Box>
+                  <Box>
+                    <Button
+                      label="Cancel"
+                      onClick={() => setShowArchive(false)}
+                    />
+                    <Button
+                      primary
+                      label="Archive Resource"
+                      onClick={async () => {
+                        const archiveRequest = await api.resource.update(
+                          resource.id,
+                          { is_archived: true },
+                          token
+                        )
+                        if (archiveRequest.isOk) {
+                          onChange()
+                        } else {
+                          addAlert('Unable to archive request', 'error')
+                        }
+                      }}
+                    />
+                  </Box>
+                </Box>
+              )}
+              {openRequests !== 0 && (
+                <Box>
+                  <Box direction="row">
+                    <Icon name="Warning" color="warning" />
+                    <Text>Cannot archive resource with open requests.</Text>
+                  </Box>
+                  <Box>
+                    <Text>
+                      Your resource has{' '}
+                      <Link href={viewResourceLink}>
+                        <Anchor
+                          label={`${openRequests} open requests`}
+                          href={viewResourceLink}
+                        />
+                      </Link>
+                      {'. '}
+                      Please accept or reject those requests before archiving
+                      this resource.
+                    </Text>
+                  </Box>
+                  <Box>
+                    <Link href={viewResourceLink}>
+                      <Button
+                        as="a"
+                        label="Show Requests"
+                        href={viewResourceLink}
+                      />
+                    </Link>
+                  </Box>
+                </Box>
+              )}
+            </>
+          </Modal>
+        </>
       )}
       {options.includes('delete') && (
-        <Anchor
-          icon={<Icon name="Trashcan" size="small" color="error" />}
-          color="error"
-          label="Delete"
-          onClick={onDelete}
-          disabled={openRequests > 0}
-        />
+        <>
+          <Anchor
+            icon={<Icon name="Trashcan" size="small" color="error" />}
+            color="error"
+            label="Delete"
+            onClick={() => setShowDelete(true)}
+            disabled={openRequests > 0}
+          />
+          <Modal showing={showDelete} setShowing={setShowDelete}>
+            <>
+              <Box
+                border={{
+                  side: 'bottom',
+                  color: 'border-black',
+                  size: 'small'
+                }}
+              >
+                <Text size="large" color="error">
+                  Delete Resource
+                </Text>
+              </Box>
+              <Box>
+                <Text>Are you sure you want to delete this resource?</Text>
+                <Text>{resource.title}</Text>
+                <Box>
+                  <Box>
+                    <Button
+                      label="Cancel"
+                      onClick={() => setShowDelete(false)}
+                    />
+                    <Button
+                      background="error"
+                      label="Delete Resource"
+                      onClick={async () => {
+                        const deleteResourceRequest = await api.resource.delete(
+                          resource.id,
+                          token
+                        )
+                        if (deleteResourceRequest.isOk) {
+                          if (onChange) onChange()
+                        } else {
+                          addAlert('Unable to delete resource', 'error')
+                        }
+                      }}
+                    />
+                  </Box>
+                </Box>
+              </Box>
+            </>
+          </Modal>
+        </>
       )}
     </Box>
   )
@@ -72,7 +198,8 @@ export const ManageOptions = ({
 export const ManageResourceCard = ({
   resource,
   options = [],
-  moreOptions = []
+  moreOptions = [],
+  onChange
 }) => {
   const { token, getTeam, isPersonalResource } = useUser()
   const manageLink = `/account/manage-resources/${resource.id}`
@@ -101,6 +228,7 @@ export const ManageResourceCard = ({
       resource={resource}
       options={moreOptions}
       openRequests={openRequests}
+      onChange={onChange}
     />
   )
 
@@ -113,6 +241,7 @@ export const ManageResourceCard = ({
             resource={resource}
             options={options}
             openRequests={openRequests}
+            onChange={onChange}
           />
           {moreOptions.length > 0 && (
             <DropButton
@@ -153,8 +282,18 @@ export const ManageResourceCard = ({
         ) : (
           <Text>0 Open Requests</Text>
         )}
-        <Box background="brand" round width="8px" height="8px" margin="small" />
-        <Text>Publicly Available</Text>
+        {!resource.is_archived && (
+          <>
+            <Box
+              background="brand"
+              round
+              width="8px"
+              height="8px"
+              margin="small"
+            />
+            <Text>Publicly Available</Text>
+          </>
+        )}
       </Box>
     </Box>
   )
