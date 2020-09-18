524bceeeca24cef6693f12e7f2b69c0913192920
Philip Jones <philipjones@ccdls-mbp.myfiosgateway.com>
2020-09-18 11:18:06 -0400

    fixed tests

--- api/resources_portal/models/frontend_notifications_config.py
+++ api/resources_portal/models/frontend_notifications_config.py
@@ -65,53 +65,53 @@ FRONTEND_NOTIFICATIONS = {
     },
     "MATERIAL_REQUEST_REQUESTER_ACCEPTED": {
         "body": (
-            "[organization_name]() has accepted your request for [material_name]()} "
+            "[organization_name]() has accepted your request for [material_name]() "
             "on the condition that you provide the following items: {required_info_plain_text}"
         )
     },
     "MATERIAL_REQUEST_REQUESTER_IN_FULFILLMENT": {
         "body": (
             "[organization_name]() has accepted your request for"
-            " [material_name]()} and is working to fulfill your request."
+            " [material_name]() and is working to fulfill your request."
         )
     },
     "MATERIAL_REQUEST_REQUESTER_EXECUTED_MTA": {
         "body": (
             "[organization_name]() has uploaded the fully executed MTA for your request for"
-            " [material_name]()} and is working to fulfill your request."
+            " [material_name]() and is working to fulfill your request."
         ),
     },
     "MATERIAL_REQUEST_REQUESTER_FULFILLED": {
         "body": (
             "[organization_name]() marked your request for"
-            " [material_name]()} as fulfilled.\nPlease view fulfilment notes for details."
+            " [material_name]() as fulfilled.\nPlease view fulfilment notes for details."
         )
     },
     "MATERIAL_REQUEST_REQUESTER_REJECTED": {
         "body": (
-            "[organization_name]() rejected your request for [material_name]()} "
+            "[organization_name]() rejected your request for [material_name]() "
             "for the below reason:<br>{rejection_reason}"
         )
     },
     "MATERIAL_REQUEST_REQUESTER_CANCELLED": {
-        "body": "You cancelled your request for [material_name]()}.",
+        "body": "You cancelled your request for [material_name]().",
     },
     "MATERIAL_REQUEST_REQUESTER_ESCALATED": {
         "body": (
-            "Request for {material_category}, [material_name]()} has been"
+            "Request for {material_category}, [material_name]() has been"
             " escalated to the grants team.<br>"
             "ALSF Grants team is looking into this and will be in touch with you soon.<br>"
             "Below is the message they received from you for your records.\n{message}"
         )
     },
     "MATERIAL_ADDED": {
-        "body": ("{other_name} added a new [material_name]()} to [organization_name]()."),
+        "body": ("{other_name} added a new [material_name]() to [organization_name]()."),
     },
     "MATERIAL_ARCHIVED": {
-        "body": ("{other_name} archived [material_name]()} from [organization_name]().")
+        "body": ("{other_name} archived [material_name]() from [organization_name]().")
     },
     "MATERIAL_DELETED": {
-        "body": ("{other_name} deleted [material_name]()} from [organization_name]()."),
+        "body": ("{other_name} deleted [material_name]() from [organization_name]()."),
     },
     "ORGANIZATION_NEW_MEMBER": {"body": "{other_name} was added to [organization_name]().",},
     "ORGANIZATION_BECAME_OWNER": {
