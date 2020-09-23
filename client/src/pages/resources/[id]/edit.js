d655d05ac692bf09eb11a3bdb00224887bc0156d
David Mejia <davidsmejia@gmail.com>
2020-09-23 16:17:28 -0400

    add edit page

---
+++ client/src/pages/resources/[id]/edit.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import dynamic from 'next/dynamic'
+import { Heading } from 'grommet'
+import { Loader } from 'components/Loader'
+import { ResourceContextProvider } from 'contexts/ResourceContext'
+import api from 'api'
+
+const ResourceEditForm = dynamic(
+  () => import('components/resources/ResourceEditForm'),
+  { ssr: false }
+)
+
+export const EditResource = ({ resourceId }) => {
+  const [editResource, setEditResource] = React.useState()
+
+  React.useEffect(() => {
+    const fetchEditResource = async () => {
+      const { isOk, response } = await api.resources.get(resourceId)
+      if (isOk) setEditResource(response)
+    }
+
+    if (!editResource) fetchEditResource()
+  })
+
+  if (!editResource) return <Loader />
+
+  return (
+    <ResourceContextProvider editResource={editResource}>
+      <Heading level={4} serif>
+        List a Resource
+      </Heading>
+      <ResourceEditForm />
+    </ResourceContextProvider>
+  )
+}
+
+EditResource.getInitialProps = ({ query: { id } }) => {
+  return {
+    resourceId: id
+  }
+}
+
+export default EditResource
