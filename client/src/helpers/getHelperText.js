0170953fdcdc3f4ed63967aa30c220aaa26e7f77
David Mejia <davidsmejia@gmail.com>
2020-09-25 10:39:47 -0400

    adds links

--- client/src/helpers/getHelperText.js
+++ client/src/helpers/getHelperText.js
@@ -4,11 +4,11 @@ export const helperTexts = {
   citation: `How do you want researchers to cite this resource?`,
   contact_user: `This person is responsible for responding to inquiries about resource details, shipping and handling, etc.`,
   organisms: `Provide scientific names of organisms`,
-  pre_print_doi: `Example: [bioRxiv](http://link) DOI`,
+  pre_print_doi: `Example: [bioRxiv](https://www.biorxiv.org/) DOI`,
   purpose: `What does this plasmid do?`,
   relevant_mutations: `Mention any mutations in the gene/insert`,
-  sequence_maps: `Upload the full sequence and partial sequence maps. You can generate sequence maps [here](http://link).`,
-  str_profile: `You obtain an STR profile for your cell line [here](http://link).`
+  sequence_maps: `Upload the full sequence and partial sequence maps. You can generate sequence maps [here](http://wishart.biology.ualberta.ca/PlasMapper/).`,
+  str_profile: `You obtain an STR profile for your cell line [here](https://www.atcc.org/en/Services/Testing_Services.aspx).`
 }
 
 export default (attribute) => {
