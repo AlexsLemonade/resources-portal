19c1bd1e0e6cd8c69794b9d70228ef4714567052
David Mejia <davidsmejia@gmail.com>
2020-10-06 15:11:09 -0400

    label optional and bold name

--- client/src/components/PreviewAddress.js
+++ client/src/components/PreviewAddress.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Box, Text } from 'grommet'
 
-export default ({ address, label = 'Shipping Address' }) => {
+export default ({ address, label = 'Shipping Address', noLabel = false }) => {
   return (
     <Box margin={{ bottom: 'medium' }}>
-      <Text weight="bold" margin={{ bottom: 'small' }}>
-        {label}
-      </Text>
-      <Text>{address.name}</Text>
+      {!noLabel && (
+        <Text weight="bold" margin={{ bottom: 'small' }}>
+          {label}
+        </Text>
+      )}
+      <Text weight="bold">{address.name}</Text>
       <Text>{address.institution}</Text>
       <Text>{address.address_line_1}</Text>
       <Text>{address.address_line_2}</Text>
