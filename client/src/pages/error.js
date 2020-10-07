3d66b299df766c7b1e7d609909db3d5a361dcffe
David Mejia <davidsmejia@gmail.com>
2020-10-07 14:29:07 -0400

    add error id to error page when sentry catches

--- client/src/pages/error.js
+++ client/src/pages/error.js
@@ -1,9 +1,27 @@
-import { Text } from 'grommet'
 import React from 'react'
+import { Box, Button, Text } from 'grommet'
+import { useRouter } from 'next/router'
 
-export const ErrorPage = ({ errorMessage, ...config }) => {
-  console.log(config)
-  return <Text>{errorMessage}</Text>
+export const ErrorPage = ({ errorMessage, eventId }) => {
+  const router = useRouter()
+  return (
+    <Box direction="row" justify="center" fill>
+      <Box direction="row" justify="center" align="center">
+        <Box align="start" width="full">
+          <Text size="xlarge" margin={{ bottom: 'large' }}>
+            Uh oh, something happened.
+          </Text>
+          {errorMessage && <Text>{errorMessage}</Text>}
+          {eventId && (
+            <Text size="small" color="critical" round="small">
+              Error ID: {eventId}
+            </Text>
+          )}
+          <Button primary label="Go Back" onClick={() => router.back()} />
+        </Box>
+      </Box>
+    </Box>
+  )
 }
 
 ErrorPage.getInitialProps = async ({ query }) => {
