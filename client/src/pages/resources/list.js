50f22dabc8706e5e26ce6b1e702bff48c60f4c51
David Mejia <davidsmejia@gmail.com>
2020-10-15 15:52:24 -0400

    remove title move to form to hide on success

--- client/src/pages/resources/list.js
+++ client/src/pages/resources/list.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import dynamic from 'next/dynamic'
-import { Heading } from 'grommet'
 import { ResourceContextProvider } from 'contexts/ResourceContext'
 
 const ResourceForm = dynamic(
@@ -12,9 +11,6 @@ export const ResourcesList = () => {
   // - prevent people with no grants
   return (
     <ResourceContextProvider localStorageName="list-resource">
-      <Heading level={4} serif>
-        List a Resource
-      </Heading>
       <ResourceForm />
     </ResourceContextProvider>
   )
