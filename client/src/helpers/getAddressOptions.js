e71dfc7d962644813c72878215de9bef69c98b25
David Mejia <davidsmejia@gmail.com>
2020-10-15 15:53:12 -0400

    refactor address options to error when null

--- client/src/helpers/getAddressOptions.js
+++ client/src/helpers/getAddressOptions.js
@@ -1,10 +1,8 @@
-export default (addresses) => {
-  const filteredAddresses = addresses.filter((a) => !!a)
-  return filteredAddresses.map((address) => ({
+export default (addresses) =>
+  addresses.map((address) => ({
     disabled: false,
     id: `${address.id}`,
     name: `address-${address.id}`,
     value: address.id,
     address
   }))
-}
