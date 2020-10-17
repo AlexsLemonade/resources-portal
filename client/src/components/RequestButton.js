5d1b39b9a410977c0619e146e7ae16ebb4419ffb
David Mejia <davidsmejia@gmail.com>
2020-10-17 12:24:23 -0400

    link imported to source repo

--- client/src/components/RequestButton.js
+++ client/src/components/RequestButton.js
@@ -3,22 +3,34 @@ import { Button } from 'grommet'
 import Link from 'next/link'
 import { CreateAccountLoginButton } from 'components/CreateAccountLoginButton'
 import { useUser } from 'hooks/useUser'
+import { getReadable } from 'helpers/readableNames'
 
 export const RequestButton = ({ resource }) => {
   const { isLoggedIn } = useUser()
 
-  if (isLoggedIn) {
-    return (
-      <Link
-        href="/resources/[id]/request"
-        as={`/resources/${resource.id}/request`}
-      >
-        <Button label="Request" primary />
-      </Link>
-    )
-  }
+  const requestLink = resource.imported
+    ? resource.url
+    : `/resources/${resource.id}/request`
 
-  return <CreateAccountLoginButton title="Sign in to Request" plainButton />
+  if (!isLoggedIn)
+    return <CreateAccountLoginButton title="Sign in to Request" plainButton />
+
+  return resource.imported ? (
+    <Button
+      as="a"
+      href={requestLink}
+      target="_blank"
+      label={`Request on ${
+        getReadable(resource.import_source) || 'Source Site'
+      }`}
+      margin={{ bottom: 'small' }}
+      primary
+    />
+  ) : (
+    <Link href={requestLink}>
+      <Button as="a" href={requestLink} label="Request" primary />
+    </Link>
+  )
 }
 
 export default RequestButton
