ce93e84cb72b372e4894c2b1f1077a89baa4a58d
David Mejia <davidsmejia@gmail.com>
2020-10-23 14:49:59 -0400

    refactor reduce complexity

--- client/src/hooks/useSignIn.js
+++ client/src/hooks/useSignIn.js
@@ -1,69 +1,17 @@
 import { useLocalStorage } from 'hooks/useLocalStorage'
 import { useRouter } from 'next/router'
 import React from 'react'
-import api from '../api'
-import { useUser } from './useUser'
+import api from 'api'
+import { useUser } from 'hooks/useUser'
 
-export const useSignIn = () => {
-  const { user, setUser, setToken } = useUser()
+export const useSignIn = (code, originUrl) => {
+  const { user, fetchUserWithNewToken } = useUser()
   const [error, setError] = React.useState('')
   const [email, setEmail] = useLocalStorage('email')
   const [needsEmail, setNeedsEmail] = useLocalStorage('needsEmail', false)
   const [orcidInfo, setOrcidInfo] = useLocalStorage('orcidInfo')
   const [clientRedirectUrl] = useLocalStorage('clientRedirectUrl', '')
   const router = useRouter()
-
-  const loginOrCreateUser = async () => {
-    const [tokenRequest, userRequest] = await api.user.login(
-      orcidInfo.orcid,
-      orcidInfo.accessToken,
-      orcidInfo.refreshToken
-    )
-
-    const userInfo = {}
-
-    if (tokenRequest.isOk) {
-      userInfo.token = tokenRequest.response.token
-
-      if (userRequest.isOk) {
-        userInfo.authenticatedUser = userRequest.response
-      }
-    } else {
-      const [creationTokenRequest, creationUserRequest] = await api.user.create(
-        orcidInfo.orcid,
-        orcidInfo.accessToken,
-        orcidInfo.refreshToken,
-        email
-      )
-
-      if (creationTokenRequest.status === 401) {
-        setError(creationTokenRequest)
-        setNeedsEmail(true)
-        return
-      }
-
-      if (!creationTokenRequest.isOk) {
-        setError(creationTokenRequest)
-        return
-      }
-
-      userInfo.token = creationTokenRequest.response.token
-
-      if (!creationUserRequest.status === 200) {
-        setError(creationUserRequest)
-        return
-      }
-
-      userInfo.authenticatedUser = creationUserRequest.response
-    }
-
-    if (userInfo.authenticatedUser && userInfo.authenticatedUser.id) {
-      setUser(userInfo.authenticatedUser)
-    }
-    if (userInfo.token) {
-      setToken(userInfo.token)
-    }
-  }
 
   const cleanup = () => {
     window.localStorage.removeItem('email')
@@ -72,14 +20,64 @@ export const useSignIn = () => {
     window.localStorage.removeItem('clientRedirectUrl')
   }
 
-  React.useEffect(() => {
-    if (orcidInfo && !user && !error && !needsEmail) {
-      loginOrCreateUser()
+  const loginOrCreateUser = async () => {
+    const orcidRequest = await api.orcid.create(code, originUrl)
+
+    if (!orcidRequest.isOk) {
+      setError(orcidRequest)
+      return
     }
-  })
+
+    const tokenRequest = await api.users.authenticate(orcidRequest.response)
+
+    if (tokenRequest.isOk) {
+      fetchUserWithNewToken(
+        tokenRequest.response.user_id,
+        tokenRequest.response.token
+      )
+    }
+
+    // the user was not able to be authenticated
+    // create them
+    if (!tokenRequest.isOk) {
+      const createUserRequest = await api.users.create(
+        orcidRequest.response,
+        email
+      )
+
+      if (createUserRequest.status === 401) {
+        setError(createUserRequest)
+        setNeedsEmail(true)
+        return
+      }
+
+      if (!createUserRequest.isOk) {
+        setError(createUserRequest)
+        return
+      }
+
+      if (!createUserRequest.status === 200) {
+        setError(createUserRequest)
+      }
+
+      const {
+        response: { user_id: userId, token: newToken }
+      } = createUserRequest
+
+      fetchUserWithNewToken(userId, newToken)
+    }
+  }
 
   React.useEffect(() => {
+    // clean up local storage on navigation
     router.events.on('routeChangeStart', cleanup)
+
+    const canLoginOrCreateUser = code && !user && !error && !needsEmail
+    if (canLoginOrCreateUser) loginOrCreateUser()
+
+    const canRedirectUser = user && !needsEmail
+    if (canRedirectUser)
+      router.replace(clientRedirectUrl || '/account/basic-information')
 
     // If the component is unmounted, unsubscribe
     // from the event with the `off` method:
@@ -87,10 +85,6 @@ export const useSignIn = () => {
       router.events.off('routeChangeStart', cleanup)
     }
   })
-
-  if (clientRedirectUrl && user && !needsEmail) {
-    router.replace(clientRedirectUrl)
-  }
 
   return {
     loginOrCreateUser,
