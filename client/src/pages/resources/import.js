37940673605e1d153408c0a65df3974ed3a7b1c2
David Mejia <davidsmejia@gmail.com>
2020-10-23 14:55:30 -0400

    add login required guards

--- client/src/pages/resources/import.js
+++ client/src/pages/resources/import.js
@@ -3,18 +3,25 @@ import dynamic from 'next/dynamic'
 import { Heading } from 'grommet'
 import { ResourceContextProvider } from 'contexts/ResourceContext'
 
+const LoginRequired = dynamic(() => import('components/LoginRequired'), {
+  ssr: false
+})
+
 const ImportResource = dynamic(
   () => import('components/resources/ImportResource'),
   { ssr: false }
 )
+
 export const ResourcesImport = () => {
   return (
-    <ResourceContextProvider localStorageName="import-resource">
-      <Heading level={4} serif>
-        Import a Resource
-      </Heading>
-      <ImportResource />
-    </ResourceContextProvider>
+    <LoginRequired title="You Must Login To Import a Resource">
+      <ResourceContextProvider localStorageName="import-resource">
+        <Heading level={4} serif>
+          Import a Resource
+        </Heading>
+        <ImportResource />
+      </ResourceContextProvider>
+    </LoginRequired>
   )
 }
 
