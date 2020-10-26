e00d61711a836b2b3423fbddf9a2bcec0ee686d9
David Mejia <davidsmejia@gmail.com>
2020-10-26 16:22:10 -0400

    clean up old comments

--- client/src/pages/account/index.js
+++ client/src/pages/account/index.js
@@ -2,18 +2,16 @@ import React from 'react'
 import { Loader } from 'components/Loader'
 import { EnterEmailModal } from 'components/modals/EnterEmailModal'
 import { useSignIn } from 'hooks/useSignIn'
-// import { useRouter } from 'next/router'
+
+// This page should only:
+// 1) finish sign up by asking for email
+// 2) let the hook redirect the user to where they left off
 
 export const Account = ({ code, originUrl }) => {
-  // const router = useRouter()
   const { needsEmail, setEmail, setNeedsEmail, setError } = useSignIn(
     code,
     originUrl
   )
-
-  // this page should only:
-  // 1) finish sign up by asking for email
-  // 2) let the hook redirect the user to where they left off
 
   if (needsEmail) {
     return (
