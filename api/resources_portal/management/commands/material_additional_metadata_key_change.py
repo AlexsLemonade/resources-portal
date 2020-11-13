782bec2df479da5097470246da6e103c998a7ac1
Philip Jones <philipjones@ccdls-mbp.myfiosgateway.com>
2020-11-13 14:53:16 -0500

    added default to add, other pr requests

--- api/resources_portal/management/commands/material_additional_metadata_key_change.py
+++ api/resources_portal/management/commands/material_additional_metadata_key_change.py
@@ -11,7 +11,7 @@ def validateCategory(category):
     return len([choice for choice in Material.CATEGORY_CHOICES if category in choice]) > 0
 
 
-def addKey(category, new_key):
+def addKey(category, new_key, default):
     querySet = None
     materials_changed = 0
 
@@ -23,13 +23,14 @@ def addKey(category, new_key):
     else:
         querySet = Material.objects.all()
 
-    with transaction.atomic():
-        for material in querySet:
-            material.additional_metadata[new_key] = ""
-            material.save()
-            materials_changed += 1
+    for material in querySet:
+        material.additional_metadata[new_key] = default
+        material.save()
+        materials_changed += 1
 
-    logger.info(f"The key '{new_key}' was added to {materials_changed} materials.")
+    logger.info(
+        f"The key '{new_key}' was added to {materials_changed} materials. The default value was '{default}'."
+    )
 
 
 def removeKey(category, old_key):
@@ -44,11 +45,10 @@ def removeKey(category, old_key):
     else:
         querySet = Material.objects.all()
 
-    with transaction.atomic():
-        for material in querySet:
-            material.additional_metadata.pop(old_key)
-            material.save()
-            materials_changed += 1
+    for material in querySet:
+        material.additional_metadata.pop(old_key)
+        material.save()
+        materials_changed += 1
 
     logger.info(f"The key '{old_key}' was removed from {materials_changed} materials.")
 
@@ -66,28 +66,28 @@ def material_additional_metadata_key_change(category, old_key, new_key):
         querySet = Material.objects.filter(additional_metadata__has_key=old_key)
 
     # Note that this doesn't use the above add and remove commands. This is because doing so would require twice the queries.
-    with transaction.atomic():
-        for material in querySet:
-            material.additional_metadata[new_key] = material.additional_metadata[old_key]
-            material.additional_metadata.pop(old_key)
-            material.save()
-            materials_changed += 1
+    for material in querySet:
+        material.additional_metadata[new_key] = material.additional_metadata.pop(old_key)
+        material.save()
+        materials_changed += 1
 
     logger.info(f"The key '{old_key}' was changed to '{new_key}' in {materials_changed} materials.")
 
 
-# This is a command to add, remove, or change the names of additional metadata fields currently in the server.
-# It has the following use cases:
-# 1. Change a metadata key
-# ./rportal migrate-metadata-keys old_key new_key --category [CATEGORY]
-# 2. Add a metadata key
-# ./rportal migrate-metadata-keys --add new_key --category [CATEGORY]
-# 2. Remove a metadata key
-# ./rportal migrate-metadata-keys --remove old_key --category [CATEGORY]
-# For each use case, the category parameter is optional, and categories should be specified in all caps, like "PLASMID" or "MODEL_ORGANISM".
-
-
 class Command(BaseCommand):
+    """
+    This is a command to add, remove, or change the names of additional metadata fields currently in the server.
+    It has the following use cases:
+    1. Change a metadata key
+    ./rportal migrate-metadata-keys old_key new_key --category [CATEGORY]
+    2. Add a metadata key
+    ./rportal migrate-metadata-keys --add new_key --category [CATEGORY] --default [DEFAULT]
+    The default is the the value that all the new keys will be initialized with.
+    2. Remove a metadata key
+    ./rportal migrate-metadata-keys --remove old_key --category [CATEGORY]
+    For each use case, the category parameter is optional, and categories should be specified in all caps, like "PLASMID" or "MODEL_ORGANISM".
+    """
+
     help = "Replaces all instances of old_key in materials (of an optional category) with new_key."
 
     def add_arguments(self, parser):
@@ -98,20 +98,21 @@ class Command(BaseCommand):
         # Optional arguments
         parser.add_argument("--category", nargs="?", default=None)
         parser.add_argument("--add", nargs="?", default=None)
+        parser.add_argument("--default", nargs="?", default="")
         parser.add_argument("--remove", nargs="?", default=None)
 
     def handle(self, *args, **options):
-        if options["add"] != None:
-            addKey(options["category"], options["add"])
+        if options["add"] is not None:
+            addKey(options["category"], options["add"], options["default"])
             return
-        if options["remove"] != None:
+        if options["remove"] is not None:
             removeKey(options["category"], options["remove"])
             return
 
-        if options["old_key"] == None:
+        if options["old_key"] is None:
             logger.error("Missing required argument oldKey")
             return
-        if options["new_key"] == None:
+        if options["new_key"] is None:
             logger.error("Missing required argument newKey")
             return
         material_additional_metadata_key_change(
