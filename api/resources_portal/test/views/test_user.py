2406c1a69d75b779db6e30aed4242d6707ed621d
David Mejia <davidsmejia@gmail.com>
2021-01-05 12:34:06 -0500

    test for 409 conflict instead of 400

--- api/resources_portal/test/views/test_user.py
+++ api/resources_portal/test/views/test_user.py
@@ -89,7 +89,7 @@ class TestUserPostTestCase(APITestCase):
 
         response = self.client.post(self.url, self.user_data)
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_409_CONFLICT)
 
 
 class TestUserDetailTestCase(APITestCase):
