d51fcfa0a8631b85b4d68452eca3f43ebb66bc0a
David Mejia <davidsmejia@gmail.com>
2021-01-12 21:27:05 -0500

    reactor layout and content

--- client/src/pages/resources/index.js
+++ client/src/pages/resources/index.js
@@ -1,12 +1,89 @@
 import React from 'react'
-import { Box, Heading, Button, Anchor, Text } from 'grommet'
+import { Box, Heading, Button, Anchor, Text, Image, Paragraph } from 'grommet'
 import Link from 'next/link'
 import { useUser } from 'hooks/useUser'
 import CreateAccountLoginButton from 'components/CreateAccountLoginButton'
+import styled from 'styled-components'
 import ListResources from '../../images/list-resources.svg'
+// Link Logos
+
+const SmallText = styled(Text)`
+  line-height: 15px;
+`
+
+const LogoLinkRow = ({ rowTitle = false, items = [] }) => {
+  return (
+    <Box width="full" margin={{ bottom: 'large' }}>
+      <Box direction="row">
+        {items.map(({ title, href, image }, i) => (
+          <Box basis="1/3" key={href} justify="end" pad={{ right: 'large' }}>
+            {(title || (i === 0 && rowTitle)) && (
+              <Text size="large" color="brand">
+                {title || rowTitle}
+              </Text>
+            )}
+            <Box
+              as="a"
+              margin={{ top: 'small' }}
+              elevation="small"
+              align="center"
+              height="100px"
+              justify="center"
+              href={href}
+              target="_blank"
+            >
+              <Image src={`/import-logos/${image}`} />
+            </Box>
+          </Box>
+        ))}
+      </Box>
+    </Box>
+  )
+}
 
 export default function ListResource() {
   const { isLoggedIn } = useUser()
+
+  const logos = {
+    sra: {
+      href: 'https://www.ncbi.nlm.nih.gov/sra',
+      image: 'sra.svg'
+    },
+    geo: {
+      href: 'https://www.ncbi.nlm.nih.gov/geo/',
+      image: 'geo.svg'
+    },
+    dbgap: {
+      href: 'https://www.ncbi.nlm.nih.gov/gap/',
+      image: 'dbgap.svg'
+    },
+    protocols: {
+      title: 'Protocols',
+      href: 'https://www.protocols.io/',
+      image: 'protocolsio.png'
+    },
+    cellLines: {
+      title: 'Cell Lines*',
+      href: 'https://www.atcc.org/Services/Deposit_Services.aspx',
+      image: 'atcc.png'
+    },
+    plasmids: {
+      title: 'Plasmids*',
+      href: 'https://www.addgene.org/',
+      image: 'logo-addgene.png'
+    },
+    mouseModels: {
+      title: 'Mouse Models*',
+      href:
+        'https://www.jax.org/jax-mice-and-services/cryo-and-strain-donation/donate-a-strain',
+      image: 'jackson.png'
+    },
+    zebrafishModels: {
+      title: 'Zebrafish Models*',
+      href: 'https://zebrafish.org/home/guide.php',
+      image: 'zirc-logo.gif'
+    }
+  }
 
   return (
     <Box width="xlarge" alignSelf="center" margin={{ bottom: 'xlarge' }}>
@@ -32,98 +109,67 @@ export default function ListResource() {
       <p>Here are a few recommended repositories to get you started:</p>
       <Box direction="row" margin={{ top: 'medium' }} justify="between">
         <Box
-          basis="2/3"
+          width="900px"
           border={{ side: 'right', size: 'small', color: 'black-tint-95' }}
-          pad={{ right: 'xxlarge' }}
+          pad={{ bottom: 'xlarge', right: 'medium' }}
         >
-          <Box direction="row" gap="large" pad={{ bottom: 'large' }}>
-            <Box>
-              <Text size="large" color="brand">
-                Cell Lines
-              </Text>
-              <Anchor href="https://www.atcc.org/Services/Deposit_Services.aspx">
-                <img alt="add resource" src="http://placehold.it/156x100" />
-              </Anchor>
-            </Box>
-            <Box>
-              <Text size="large" color="brand">
-                Plasmid
-              </Text>
-              <Anchor href="https://www.atcc.org/Services/Deposit_Services.aspx">
-                <img alt="add resource" src="http://placehold.it/156x100" />
-              </Anchor>
-            </Box>
-            <Box>
-              <Text size="large" color="brand">
-                Protocols
-              </Text>
-              <Anchor href="https://www.atcc.org/Services/Deposit_Services.aspx">
-                <img alt="add resource" src="http://placehold.it/156x100" />
-              </Anchor>
-            </Box>
-          </Box>
-          <Box
-            direction="row"
-            gap="large"
-            pad={{ bottom: 'large' }}
-            align="end"
-          >
-            <Box>
-              <Text size="large" color="brand">
-                Datasets
-              </Text>
-              <Anchor href="https://www.atcc.org/Services/Deposit_Services.aspx">
-                <img alt="add resource" src="http://placehold.it/156x100" />
-              </Anchor>
-            </Box>
-            <Box>
-              <Anchor href="https://www.atcc.org/Services/Deposit_Services.aspx">
-                <img alt="add resource" src="http://placehold.it/156x100" />
-              </Anchor>
-            </Box>
-            <Box>
-              <Anchor href="https://www.atcc.org/Services/Deposit_Services.aspx">
-                <img alt="add resource" src="http://placehold.it/156x100" />
-              </Anchor>
-            </Box>
-          </Box>
-          <Box direction="row" gap="large" pad={{ bottom: 'large' }}>
-            <Box>
-              <Text size="large" color="brand">
-                Mouse Models
-              </Text>
-              <Anchor href="https://www.atcc.org/Services/Deposit_Services.aspx">
-                <img alt="add resource" src="http://placehold.it/156x100" />
-              </Anchor>
-            </Box>
-            <Box>
-              <Text size="large" color="brand">
-                Zebrafish Models
-              </Text>
-              <Anchor href="https://www.atcc.org/Services/Deposit_Services.aspx">
-                <img alt="add resource" src="http://placehold.it/156x100" />
-              </Anchor>
-            </Box>
+          <LogoLinkRow
+            rowTitle="Datasets"
+            items={[logos.sra, logos.geo, logos.dbgap]}
+          />
+          <LogoLinkRow
+            items={[logos.protocols, logos.cellLines, logos.plasmids]}
+          />
+          <LogoLinkRow items={[logos.mouseModels, logos.zebrafishModels]} />
+          <Box>
+            <Paragraph>
+              <Text weight="bold">*Limited support for importing</Text>
+            </Paragraph>
+            <SmallText size="small">
+              You can still add resources from these repositories, but we will
+              not be able to extract metadata from the respective repositories.
+              You will be prompted to fill out some basic information about the
+              resource so that it is discoverable.
+            </SmallText>
           </Box>
         </Box>
         <Box justify="center" pad={{ left: 'xxlarge' }}>
           {isLoggedIn && (
-            <Box align="center" pad={{ bottom: 'xlarge' }}>
-              <Text
-                size="large"
-                textAlign="center"
-                weight="normal"
-                margin={{ bottom: 'medium' }}
+            <>
+              <Box
+                align="center"
+                elevation="small"
+                pad={{ horizontal: 'small', vertical: 'large' }}
               >
-                Already listed your resource in a repository?
-              </Text>
-              <Link href="/resources/import">
-                <Button label="Import" primary />
-              </Link>
-            </Box>
+                <Paragraph
+                  size="large"
+                  textAlign="center"
+                  margin={{ bottom: 'medium' }}
+                >
+                  Already listed your resource in a repository?
+                </Paragraph>
+                <Link href="/resources/import">
+                  <Button label="Import" primary />
+                </Link>
+              </Box>
+              <Box
+                width="medium"
+                margin={{ top: 'xlarge' }}
+                round="xsmall"
+                alignSelf="center"
+              >
+                <Text>
+                  Repository for your resource doesn’t exist? Can’t list your
+                  resource in a repository yet?{' '}
+                  <Link href="/resources/list">
+                    <Anchor label="List with us." />
+                  </Link>
+                </Text>
+              </Box>
+            </>
           )}
           {!isLoggedIn && (
-            <Box align="center" pad="large" elevation="medium" width="330px">
+            <Box align="center" pad="large" elevation="medium">
               <Text
                 size="medium"
                 textAlign="center"
@@ -140,25 +186,6 @@ export default function ListResource() {
           )}
         </Box>
       </Box>
-      {isLoggedIn && (
-        <Box
-          width="medium"
-          margin={{ top: 'xlarge' }}
-          pad="medium"
-          round="xsmall"
-          border
-          alignSelf="center"
-          elevation="small"
-        >
-          <Text>
-            Repository for your resource doesn’t exist? Can’t list your resource
-            in a repository yet?{' '}
-            <Link href="/resources/list">
-              <Anchor label="List with us." />
-            </Link>
-          </Text>
-        </Box>
-      )}
     </Box>
   )
 }
