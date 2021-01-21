a2491846073e6b71c61a826a8f98813e9abd8968
David Mejia <davidsmejia@gmail.com>
2021-01-21 11:34:32 -0500

    treat defined as required

--- client/src/helpers/getOptionalAttributes.js
+++ client/src/helpers/getOptionalAttributes.js
@@ -1,10 +1,12 @@
 // converts a schema to a list of optional attributes
-
 export default (schema, object) => {
   const optionalAttributes = []
 
   const addRequired = ([key, desc]) => {
-    if (key && !desc.tests.find((t) => t.name === 'required')) {
+    if (
+      key &&
+      !desc.tests.find((t) => ['defined', 'required'].includes(t.name))
+    ) {
       optionalAttributes.push(key)
     }
     if (desc.fields) Object.entries(desc.fields).forEach(addRequired)
