b5e3f933771f724444e00543f6d680dd5fc52bc2
David Mejia <davidsmejia@gmail.com>
2021-01-27 10:53:27 -0500

    use RequestContextProvider

--- client/src/pages/account/requests/[id]/index.js
+++ client/src/pages/account/requests/[id]/index.js
@@ -1,31 +1,19 @@
 import React from 'react'
+import dynamic from 'next/dynamic'
 import { DrillDownNav } from 'components/DrillDownNav'
-import { useUser } from 'hooks/useUser'
-import RequesterManageRequestForm from 'components/resources/RequesterManageRequestForm'
-import SharerManageRequestForm from 'components/resources/SharerManageRequestForm'
-import api from 'api'
-import { Loader } from 'components/Loader'
+import { RequestContextProvider } from 'contexts/RequestContext'
+
+const ManageRequestForm = dynamic(
+  () => import('components/ManageRequestForm'),
+  { ssr: false }
+)
 
 export const Request = ({ id }) => {
-  const { user, token } = useUser()
-  const [request, setRequest] = React.useState()
-
-  React.useEffect(() => {
-    const fetchResourceRequest = async () => {
-      const requestRequest = await api.requests.get(id, token)
-      if (requestRequest.isOk) setRequest(requestRequest.response)
-    }
-    if (!request) fetchResourceRequest()
-  })
-
-  if (!request) return <Loader />
-
-  const isRequester = request.requester.id === user.id
-
   return (
     <DrillDownNav title="Request Details" linkBack="/account/requests">
-      {isRequester && <RequesterManageRequestForm request={request} />}
-      {!isRequester && <SharerManageRequestForm request={request} />}
+      <RequestContextProvider requestId={id}>
+        <ManageRequestForm />
+      </RequestContextProvider>
     </DrillDownNav>
   )
 }
