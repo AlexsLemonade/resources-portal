914b859d8c2de9b1182a00007646fd48cb78a56a
David Mejia <davidsmejia@gmail.com>
2021-02-08 11:03:39 -0500

    add verified and sharer states

--- client/src/components/RequestFulfilledVerified.js
+++ client/src/components/RequestFulfilledVerified.js
@@ -2,30 +2,42 @@ import React from 'react'
 import { Box, Text } from 'grommet'
 import { HeaderRow } from 'components/HeaderRow'
 import useRequest from 'hooks/useRequest'
+import PreviewFulfillmentNote from 'components/PreviewFulfillmentNote'
 
 export default () => {
-  const { request } = useRequest()
+  const { request, isRequester } = useRequest()
+  const {
+    fulfillment_notes: notes,
+    requester: { full_name: requesterName }
+  } = request
+
+  if (isRequester)
+    return (
+      <>
+        <Box pad={{ bottom: 'large' }}>
+          <Text textAlign="center">
+            You have confirmed reciept of this resource. Please look at the
+            fulfillment note for details.
+          </Text>
+        </Box>
+        <Box margin={{ vertical: 'medium' }}>
+          <HeaderRow label="Request Materials" />
+          <Text weight="bold">Fulfullment Note</Text>
+          {notes.map((note) => (
+            <PreviewFulfillmentNote key={note.id} note={note} />
+          ))}
+          {notes.length === 0 && (
+            <Text color="black-tint-60" italic>
+              There are no notes.
+            </Text>
+          )}
+        </Box>
+      </>
+    )
 
   return (
-    <>
-      <Box pad={{ bottom: 'large' }}>
-        <Text textAlign="center">
-          You have confirmed reciept of this resource. Please look at the
-          fulfillment note for details.
-        </Text>
-      </Box>
-      <Box margin={{ vertical: 'medium' }}>
-        <HeaderRow label="Request Materials" />
-        <Text weight="bold">Fulfullment Note</Text>
-        {request.fulfillment_notes.map((note) => (
-          <Text key={note.id}>{note.text}</Text>
-        ))}
-        {request.fulfillment_notes.length === 0 && (
-          <Text color="black-tint-60" italic>
-            There are no notes.
-          </Text>
-        )}
-      </Box>
-    </>
+    <Box direction="row" align="center" pad="medium">
+      <Text>{requesterName} has confirmed the receipt of this resource.</Text>
+    </Box>
   )
 }
