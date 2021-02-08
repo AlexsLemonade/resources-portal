75a023d4e2e0a37c13b3bb57468a1980c92073ca
Kurt Wheeler <kurt.wheeler91@gmail.com>
2021-02-08 17:42:44 -0500

    Fix the permissions for the material grants route. Org members should be able to associate the grants.

--- api/resources_portal/views/grant_material.py
+++ api/resources_portal/views/grant_material.py
@@ -9,14 +9,7 @@ from resources_portal.models import Grant, GrantMaterialAssociation, Material
 from resources_portal.serializers import MaterialRelationSerializer
 
 
-class OwnsGrant(BasePermission):
-    def has_permission(self, request, view):
-        grant = Grant.objects.get(pk=view.kwargs["parent_lookup_grants"])
-
-        return request.user == grant.user
-
-
-class OwnsGrantAndMaterial(BasePermission):
+class CanAccessGrantAndMaterial(BasePermission):
     def has_permission(self, request, view):
         grant = Grant.objects.get(pk=view.kwargs["parent_lookup_grants"])
         material = Material.objects.get(pk=request.data["id"])
@@ -26,9 +19,24 @@ class OwnsGrantAndMaterial(BasePermission):
         # use it rather than querying for it a second time in
         # OwnsGrant.
         return (
-            request.user == grant.user
-            and request.user in material.organization.members.all()
+            request.user in material.organization.members.all()
             and grant in material.organization.grants.all()
+        )
+
+
+class CanAddMaterialToGrant(BasePermission):
+    def has_permission(self, request, view):
+        grant = Grant.objects.get(pk=view.kwargs["parent_lookup_grants"])
+        material = Material.objects.get(pk=request.data["id"])
+
+        # Check the grant here instead of with OwnsGrant because we
+        # need to query for the Grant here anyway so we may as well
+        # use it rather than querying for it a second time in
+        # OwnsGrant.
+        return (
+            request.user in material.organization.members.all()
+            and grant in material.organization.grants.all()
+            and request.user.has_perm("add_resources", material.organization)
         )
 
 
@@ -45,9 +53,9 @@ class GrantMaterialViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
 
     def get_permissions(self):
         if self.action == "create":
-            permission_classes = [IsAuthenticated, OwnsGrantAndMaterial]
+            permission_classes = [IsAuthenticated, CanAddMaterialToGrant]
         else:
-            permission_classes = [IsAuthenticated, OwnsGrant]
+            permission_classes = [IsAuthenticated, CanAccessGrantAndMaterial]
 
         return [permission() for permission in permission_classes]
 
