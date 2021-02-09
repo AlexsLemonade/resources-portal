3da36bf31fffb3b974537ed2220cb25512b1a403
David Mejia <davidsmejia@gmail.com>
2021-02-09 10:50:51 -0500

    add year to grant

--- api/resources_portal/models/grant.py
+++ api/resources_portal/models/grant.py
@@ -22,6 +22,7 @@ class Grant(SafeDeleteModel):
     updated_at = models.DateTimeField(auto_now=True)
 
     title = models.TextField()
+    year = models.PositiveSmallIntegerField(null=True, blank=False)
     funder = models.CharField(max_length=80, default="ALSF")
     funder_id = models.CharField(max_length=80)
 
