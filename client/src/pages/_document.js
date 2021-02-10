f09e58f13f49f6db012e916cd3bbb2150e12febf
David Mejia <davidsmejia@gmail.com>
2021-02-10 12:20:11 -0500

    add google analytics and hotjar code

--- client/src/pages/_document.js
+++ client/src/pages/_document.js
@@ -18,7 +18,26 @@ export default class MyDocument extends Document {
     return (
       <html lang="en-US">
         <Head>
-          {/* Step 5: Output the styles in the head  */}
+          {/* Google Analytics */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-BYH3X5WS0V"
+          />
+          <script
+            async
+            dangerouslySetInnterHTML={{
+              __html:
+                "window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);} gtag('js', new Date());gtag('config', 'G-BYH3X5WS0V');"
+            }}
+          />
+          {/* Hotjar */}
+          <script
+            async
+            dangerouslySetInnterHTML={{
+              __html:
+                "(function(h,o,t,j,a,r){h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};h._hjSettings={hjid:2241444,hjsv:6};a=o.getElementsByTagName('head')[0];r=o.createElement('script');r.async=1;r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;a.appendChild(r);})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');"
+            }}
+          />
           {this.props.styleTags}
         </Head>
         <body>
