93ed6918dbf27da3c791400bdd02d37741ec1acd
David Mejia <davidsmejia@gmail.com>
2021-02-12 16:31:03 -0500

    fix fallback implementation

--- client/src/pages/_error.js
+++ client/src/pages/_error.js
@@ -1,26 +1,50 @@
 import React from 'react'
-import { Box, Button, Text } from 'grommet'
+import { Box, Button, Grommet, Text } from 'grommet'
 import { useRouter } from 'next/router'
+import theme from 'theme'
 
-export const ErrorPage = ({ errorMessage, eventId }) => {
+const ErrorPage = ({ sentry = {} }) => {
   const router = useRouter()
+
+  const { eventId } = sentry
+
+  React.useEffect(() => {
+    const forceRefresh = (url) => {
+      window.location = url
+    }
+
+    router.events.on('routeChangeStart', forceRefresh)
+  }, [])
+
+  const goBack = () => {
+    router.back()
+  }
+
   return (
-    <Box direction="row" justify="center" fill>
-      <Box direction="row" justify="center" align="center">
-        <Box align="start" width="full">
-          <Text size="xlarge" margin={{ bottom: 'large' }}>
-            Uh oh, something happened.
-          </Text>
-          {errorMessage && <Text>{errorMessage}</Text>}
-          {eventId && (
-            <Text size="small" color="error" round="small">
-              Error ID: {eventId}
+    <Grommet theme={theme} full>
+      <Box justify="center" fill>
+        <Box direction="row">
+          <Box align="center" width="full">
+            <Text size="xlarge" margin={{ bottom: 'large' }}>
+              Uh oh, something happened.
             </Text>
-          )}
-          <Button primary label="Go Back" onClick={() => router.back()} />
+            <Text>
+              We have been notified and are taking a look at the issue.
+            </Text>
+            {eventId && (
+              <Box margin={{ vertical: 'medium' }}>
+                <Text size="small" color="error" round="small">
+                  Error ID: {eventId}
+                </Text>
+              </Box>
+            )}
+            <Box direction="row" justify="center" width="full">
+              <Button primary label="Go Back" onClick={goBack} />
+            </Box>
+          </Box>
         </Box>
       </Box>
-    </Box>
+    </Grommet>
   )
 }
 
