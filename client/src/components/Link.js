6af6034b2bf36849aed3d3f1c971ea1010a1ad19
David Mejia <davidsmejia@gmail.com>
2021-02-12 12:19:43 -0500

    use link that wraps the Anchor for client side transitions

---
+++ client/src/components/Link.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+import { Anchor } from 'grommet'
+
+export default ({ href, label, children }) => {
+  return (
+    <Link href={href} prefetch>
+      <Anchor href={href} label={label}>
+        {children}
+      </Anchor>
+    </Link>
+  )
+}
