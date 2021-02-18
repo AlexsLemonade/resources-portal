99fd79d1d346728ba1bc94cf92e131402878ec0e
David Mejia <davidsmejia@gmail.com>
2021-02-18 16:03:57 -0500

    fix typo

--- client/src/pages/_document.js
+++ client/src/pages/_document.js
@@ -26,7 +26,8 @@ export default class MyDocument extends Document {
           />
           <script
             async
-            dangerouslySetInnterHTML={{
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
               __html:
                 "window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);} gtag('js', new Date());gtag('config', 'G-BYH3X5WS0V');"
             }}
@@ -34,7 +35,8 @@ export default class MyDocument extends Document {
           {/* Hotjar */}
           <script
             async
-            dangerouslySetInnterHTML={{
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
               __html:
                 "(function(h,o,t,j,a,r){h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};h._hjSettings={hjid:2241444,hjsv:6};a=o.getElementsByTagName('head')[0];r=o.createElement('script');r.async=1;r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;a.appendChild(r);})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');"
             }}
