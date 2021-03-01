4dfc3a5f1aaeccb8395463c0ba62082a20c23192
David Mejia <davidsmejia@gmail.com>
2021-03-01 17:43:16 -0500

    load any open requests and prevent resource transfer

--- client/src/components/GrantRemoveButton.js
+++ client/src/components/GrantRemoveButton.js
@@ -3,10 +3,15 @@ import { Box, Paragraph, Text } from 'grommet'
 import RadioButtonGroup from 'components/RadioButtonGroup'
 import Button from 'components/Button'
 import { Modal } from 'components/Modal'
+import { List, ListItem } from 'components/List'
+import { InfoCard } from 'components/InfoCard'
 import Icon from 'components/Icon'
 import useTeamForm from 'hooks/useTeamForm'
+import { useUser } from 'hooks/useUser'
+import api from 'api'
 
 export default ({ grant }) => {
+  const { token } = useUser()
   const options = [
     {
       label: 'Remove Grant Only',
@@ -48,6 +53,39 @@ export default ({ grant }) => {
   const resourcesCount = grantResources.length
   const hasGrantResources = resourcesCount > 0
   const hasManyResources = resourcesCount > 1
+  const [requestedResources, setRequestedResources] = React.useState([])
+  const hasOpenRequests = requestedResources.length > 0
+  const fetchedRef = React.useRef(false)
+
+  React.useEffect(() => {
+    const fetchRequests = async () => {
+      fetchedRef.current = true
+      // check for team and grant
+      const requestRequests = await Promise.all(
+        grantResources.map((resource) =>
+          api.resources.requests.filter(
+            resource.id,
+            {
+              is_active_sharer: true
+            },
+            token
+          )
+        )
+      )
+
+      // fix any requests that didn't respond correctly
+      // we should probably consider preventing the removing of grants
+      // if this is the case
+      const requestCounts = requestRequests.map((r) =>
+        r.isOk ? r.response.count : 0
+      )
+
+      setRequestedResources(
+        grantResources.filter((r, i) => requestCounts[i] > 0)
+      )
+    }
+    if (!fetchedRef.current) fetchRequests()
+  })
 
   return (
     <>
@@ -65,62 +103,84 @@ export default ({ grant }) => {
         critical
         title="Remove Grant"
       >
-        {hasGrantResources &&
-          (showOptions ? (
-            <>
-              <Box width="large">
-                <Text margin={{ vertical: 'medium' }}>
-                  {name} has {resourcesCount} resource{hasManyResources && 's'}{' '}
-                  associated with {grant.title}.
-                </Text>
-                <RadioButtonGroup
-                  value={transfer}
-                  options={options}
-                  onChange={({ target: { value: newValue } }) => {
-                    setTransfer(newValue)
-                  }}
-                />
-              </Box>
-              <Box direction="row" justify="end" gap="medium">
-                <Button label="Cancel" onClick={close} />
-                <Button
-                  critical
-                  label="Remove Grant"
-                  onClick={() => setShowOptions(false)}
-                />
-              </Box>
-            </>
-          ) : (
-            <>
-              <Box margin={{ vertical: 'medium' }}>
-                <Paragraph>
-                  Are you sure you want to remove{' '}
-                  <Text weight="bold">{grant.title}</Text>
-                  {transfer === 'true' && " and all it's resources"} from {name}
-                  ?
-                </Paragraph>
-                <Paragraph>
-                  {transfer === 'false' ? (
-                    <Text weight="bold">
-                      Members of the team can no longer be able to associate new
-                      resources with this grant but can continue to manage
-                      exisiting resources associated with this grant.
-                    </Text>
-                  ) : (
-                    <Text weight="bold">
-                      Members of the team can no longer manage resources
-                      associated with this grant. The Grant Holder wil be
-                      responsible for managing these resources.
-                    </Text>
-                  )}
-                </Paragraph>
-              </Box>
-              <Box direction="row" justify="end" gap="medium">
-                <Button label="Back" onClick={() => setShowOptions(true)} />
-                <Button critical label="Yes" onClick={removeGrantAndRefresh} />
-              </Box>
-            </>
-          ))}
+        {hasGrantResources && showOptions && (
+          <>
+            <Box width="large">
+              <Text margin={{ vertical: 'medium' }}>
+                {name} has {resourcesCount} resource{hasManyResources && 's'}{' '}
+                associated with {grant.title}.
+              </Text>
+              <RadioButtonGroup
+                name={`remove-grant-option-${grant.id}`}
+                value={transfer}
+                options={options}
+                onChange={({ target: { value: newValue } }) => {
+                  setTransfer(newValue)
+                }}
+              />
+            </Box>
+            <Box direction="row" justify="end" gap="medium">
+              <Button label="Cancel" onClick={close} />
+              <Button
+                critical
+                label="Remove Grant"
+                onClick={() => setShowOptions(false)}
+              />
+            </Box>
+          </>
+        )}
+        {!showOptions && hasOpenRequests && (
+          <>
+            <InfoCard type="Warning" iconColor="error" elevation="none">
+              <Text>
+                We cannot transfer resources associated with the grant{' '}
+                {grant.title} while there are active requests. Please ensure all
+                of these requests are closed before removing the grant.
+              </Text>
+            </InfoCard>
+            <Text weight="bold" margin={{ top: 'medium' }}>
+              The following resources have active requests:
+            </Text>
+            <List>
+              {requestedResources.map((r) => (
+                <ListItem key={r.id} text={r.title} />
+              ))}
+            </List>
+            <Box direction="row" justify="end" gap="medium">
+              <Button label="Back" onClick={() => setShowOptions(true)} />
+            </Box>
+          </>
+        )}
+        {!showOptions && !hasOpenRequests && (
+          <>
+            <Box margin={{ vertical: 'medium' }}>
+              <Paragraph>
+                Are you sure you want to remove{' '}
+                <Text weight="bold">{grant.title}</Text>
+                {transfer === 'true' && " and all it's resources"} from {name}?
+              </Paragraph>
+              <Paragraph>
+                {transfer === 'false' ? (
+                  <Text weight="bold">
+                    Members of the team can no longer be able to associate new
+                    resources with this grant but can continue to manage
+                    exisiting resources associated with this grant.
+                  </Text>
+                ) : (
+                  <Text weight="bold">
+                    Members of the team can no longer manage resources
+                    associated with this grant. The Grant Holder wil be
+                    responsible for managing these resources.
+                  </Text>
+                )}
+              </Paragraph>
+            </Box>
+            <Box direction="row" justify="end" gap="medium">
+              <Button label="Back" onClick={() => setShowOptions(true)} />
+              <Button critical label="Yes" onClick={removeGrantAndRefresh} />
+            </Box>
+          </>
+        )}
         {!hasGrantResources && (
           <>
             <Box margin={{ vertical: 'medium' }}>
