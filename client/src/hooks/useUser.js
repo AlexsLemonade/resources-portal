e546f7e6e4dbf440083fe3c821bc726468b46609
David Mejia <davidsmejia@gmail.com>
2021-03-19 15:46:52 -0400

    add helper for determining if user can add resource

--- client/src/hooks/useUser.js
+++ client/src/hooks/useUser.js
@@ -110,6 +110,26 @@ export const useUser = (defaultUser, defaultToken) => {
     return request.assigned_to.id === user.id
   }
 
+  const isAbleToAddResources = async () => {
+    if (!isLoggedIn) return false
+
+    const { response: freshUser, isOk: userIsOk } = await api.users.get(
+      user.id,
+      token
+    )
+
+    if (!userIsOk) return false
+
+    // user has grants
+    const { grants, organizations: teams } = freshUser
+
+    const hasGrants = grants.length > 0
+    // join all the arrays of org grants to one array and check if empty
+    const hasTeamGrants = [].concat(...teams.map((t) => t.g)).length > 0
+
+    return hasGrants || hasTeamGrants
+  }
+
   return {
     user,
     setUser,
@@ -124,6 +144,7 @@ export const useUser = (defaultUser, defaultToken) => {
     getTeam,
     isPersonalResource,
     isResourceRequester,
-    isAssignedRequest
+    isAssignedRequest,
+    isAbleToAddResources
   }
 }
