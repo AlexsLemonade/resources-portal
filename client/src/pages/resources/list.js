e343476f5a88cede77c5715208f3151157d5eedd
David Mejia <davidsmejia@gmail.com>
2021-03-19 15:49:39 -0400

    add guard to show form for feature request

--- client/src/pages/resources/list.js
+++ client/src/pages/resources/list.js
@@ -2,6 +2,9 @@ import React from 'react'
 import dynamic from 'next/dynamic'
 import { ResourceContextProvider } from 'contexts/ResourceContext'
 
+const GrantRequired = dynamic(() => import('components/GrantRequired'), {
+  ssr: false
+})
 const LoginRequired = dynamic(() => import('components/LoginRequired'), {
   ssr: false
 })
@@ -11,13 +14,13 @@ const ResourceForm = dynamic(
   { ssr: false }
 )
 export const ResourcesList = () => {
-  // TODO:
-  // - prevent people with no grants
   return (
     <LoginRequired title="You Must Login To List a Resource">
-      <ResourceContextProvider localStorageName="list-resource">
-        <ResourceForm />
-      </ResourceContextProvider>
+      <GrantRequired>
+        <ResourceContextProvider localStorageName="list-resource">
+          <ResourceForm />
+        </ResourceContextProvider>
+      </GrantRequired>
     </LoginRequired>
   )
 }
