5d13bec6f8caec25b1b5abe62584aaad619694de
David Mejia <davidsmejia@gmail.com>
2021-05-15 12:35:15 -0400

    initial pass at restructuring the add resources page

--- client/src/pages/resources/index.js
+++ client/src/pages/resources/index.js
@@ -1,89 +1,82 @@
 import React from 'react'
-import { Box, Heading, Button, Anchor, Text, Image, Paragraph } from 'grommet'
-import Link from 'next/link'
-import { useUser } from 'hooks/useUser'
-import CreateAccountLoginButton from 'components/CreateAccountLoginButton'
+import dynamic from 'next/dynamic'
+import { Box, Heading, Text, Paragraph } from 'grommet'
+import Link from 'components/Link'
 import styled from 'styled-components'
 import ListResources from '../../images/list-resources.svg'
-// Link Logos
+
+const ResourceImportListOptions = dynamic(
+  () => import('components/ResourceImportListOptions'),
+  { ssr: false }
+)
 
 const SmallText = styled(Text)`
   line-height: 15px;
 `
 
-const LogoLinkRow = ({ rowTitle = false, items = [] }) => {
-  return (
-    <Box width="full" margin={{ bottom: 'large' }}>
-      <Box direction="row">
-        {items.map(({ title, href, image }, i) => (
-          <Box basis="1/3" key={href} justify="end" pad={{ right: 'large' }}>
-            {(title || (i === 0 && rowTitle)) && (
-              <Text size="large" color="brand">
-                {title || rowTitle}
-              </Text>
-            )}
-            <Box
-              as="a"
-              margin={{ top: 'small' }}
-              elevation="small"
-              align="center"
-              height="100px"
-              justify="center"
-              href={href}
-              target="_blank"
-            >
-              <Image src={`/import-logos/${image}`} />
-            </Box>
-          </Box>
-        ))}
-      </Box>
-    </Box>
-  )
-}
-
 export default function ListResource() {
-  const { isLoggedIn } = useUser()
-
-  const logos = {
-    sra: {
-      href: 'https://www.ncbi.nlm.nih.gov/sra',
-      image: 'sra.svg'
+  const supportedRepos = [
+    {
+      repo: 'Datasets',
+      repos: [
+        { label: 'GEO', href: 'https://www.ncbi.nlm.nih.gov/geo/' },
+        {
+          label: 'SRA',
+          href: 'https://www.ncbi.nlm.nih.gov/sra'
+        },
+        {
+          label: 'dbGaP',
+          href: 'https://www.ncbi.nlm.nih.gov/gap/'
+        }
+      ]
     },
-    geo: {
-      href: 'https://www.ncbi.nlm.nih.gov/geo/',
-      image: 'geo.svg'
+    {
+      repo: 'Protocols',
+      repos: [
+        {
+          label: 'Protocols',
+          href: 'https://www.protocols.io/'
+        }
+      ]
     },
-    dbgap: {
-      href: 'https://www.ncbi.nlm.nih.gov/gap/',
-      image: 'dbgap.svg'
+    {
+      repo: 'Cell Lines*',
+      repos: [
+        {
+          label: 'ATCC',
+          href: 'https://www.atcc.org/Services/Deposit_Services.aspx'
+        }
+      ]
     },
-    protocols: {
-      title: 'Protocols',
-      href: 'https://www.protocols.io/',
-      image: 'protocolsio.png'
+    {
+      repo: 'Mouse Models*',
+      repos: [
+        {
+          label: 'Mouse Models*',
+          href:
+            'https://www.jax.org/jax-mice-and-services/cryo-and-strain-donation/donate-a-strain'
+        }
+      ]
     },
-    cellLines: {
-      title: 'Cell Lines*',
-      href: 'https://www.atcc.org/Services/Deposit_Services.aspx',
-      image: 'atcc.png'
+    {
+      repo: 'Zebrafish Models*',
+      repos: [
+        {
+          label: 'Zebrafish Models*',
+          href: 'https://zebrafish.org/home/guide.php'
+        }
+      ]
     },
-    plasmids: {
-      title: 'Plasmids*',
-      href: 'https://www.addgene.org/',
-      image: 'logo-addgene.png'
-    },
-    mouseModels: {
-      title: 'Mouse Models*',
-      href:
-        'https://www.jax.org/jax-mice-and-services/cryo-and-strain-donation/donate-a-strain',
-      image: 'jackson.png'
-    },
-    zebrafishModels: {
-      title: 'Zebrafish Models*',
-      href: 'https://zebrafish.org/home/guide.php',
-      image: 'zirc-logo.gif'
+    {
+      repo: 'Plasmids*',
+      repos: [
+        {
+          label: 'Add Gene*',
+          href: 'https://www.addgene.org/'
+        }
+      ]
     }
-  }
+  ]
 
   return (
     <Box width="xlarge" alignSelf="center" margin={{ bottom: 'xlarge' }}>
@@ -105,22 +98,24 @@ export default function ListResource() {
           <ListResources />
         </Box>
       </Box>
-
-      <p>Here are a few recommended repositories to get you started:</p>
-      <Box direction="row" margin={{ top: 'medium' }} justify="between">
-        <Box
-          width="900px"
-          border={{ side: 'right', size: 'small', color: 'black-tint-95' }}
-          pad={{ bottom: 'xlarge', right: 'medium' }}
-        >
-          <LogoLinkRow
-            rowTitle="Datasets"
-            items={[logos.sra, logos.geo, logos.dbgap]}
-          />
-          <LogoLinkRow
-            items={[logos.protocols, logos.cellLines, logos.plasmids]}
-          />
-          <LogoLinkRow items={[logos.mouseModels, logos.zebrafishModels]} />
+      <ResourceImportListOptions />
+      <Box margin={{ vertical: 'large' }}>
+        <Text serif size="large" margin={{ bottom: 'medium' }}>
+          Supported Repositories
+        </Text>
+        <Box direction="row" justify="between">
+          {supportedRepos.map(({ repo, repos }) => (
+            <Box key={repo}>
+              <Text weight="bold" margin={{ bottom: 'small' }}>
+                {repo}
+              </Text>
+              {repos.map(({ label, href }) => (
+                <Link key={href} href={href} label={label} />
+              ))}
+            </Box>
+          ))}
+        </Box>
+        <Box width="medium" margin={{ top: 'large' }}>
           <Box>
             <Paragraph>
               <Text weight="bold">*Limited support for importing</Text>
@@ -132,58 +127,6 @@ export default function ListResource() {
               resource so that it is discoverable.
             </SmallText>
           </Box>
-        </Box>
-        <Box justify="center" pad={{ left: 'xxlarge' }}>
-          {isLoggedIn && (
-            <>
-              <Box
-                align="center"
-                elevation="small"
-                pad={{ horizontal: 'small', vertical: 'large' }}
-              >
-                <Paragraph
-                  size="large"
-                  textAlign="center"
-                  margin={{ bottom: 'medium' }}
-                >
-                  Already listed your resource in a repository?
-                </Paragraph>
-                <Link href="/resources/import">
-                  <Button label="Import" primary />
-                </Link>
-              </Box>
-              <Box
-                width="medium"
-                margin={{ top: 'xlarge' }}
-                round="xsmall"
-                alignSelf="center"
-              >
-                <Text>
-                  Repository for your resource doesn’t exist? Can’t list your
-                  resource in a repository yet?{' '}
-                  <Link href="/resources/list">
-                    <Anchor label="List with us." />
-                  </Link>
-                </Text>
-              </Box>
-            </>
-          )}
-          {!isLoggedIn && (
-            <Box align="center" pad="large" elevation="medium">
-              <Text
-                size="medium"
-                textAlign="center"
-                weight="bold"
-                margin={{ bottom: 'medium' }}
-              >
-                Sign in / Create Account to List or Import Resources
-              </Text>
-              <CreateAccountLoginButton
-                title="Sign in/ Create Account"
-                plainButton
-              />
-            </Box>
-          )}
         </Box>
       </Box>
     </Box>
