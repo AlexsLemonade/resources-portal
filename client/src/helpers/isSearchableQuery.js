c8fe14ee45e55aee4aa17e918fba6f5547ef45c4
David Mejia <davidsmejia@gmail.com>
2021-06-04 14:41:21 -0400

    perform search with any params

--- client/src/helpers/isSearchableQuery.js
+++ client/src/helpers/isSearchableQuery.js
@@ -1,8 +1,5 @@
 // if a category is selected only search is a search term is provided
 
 export default (query) => {
-  if (!query) return false
-  if (query.category && !query.search) return false
-  if (Object.keys(query).length === 0) return false
-  return true
+  return query || Object.keys(query).length !== 0
 }
