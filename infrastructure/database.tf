41a0cc9d603d57f92d596f598e18e1708c0fb8c3
David <davidsmejia@gmail.com>
2022-03-29 16:07:27 -0400

    Revert "935 upgrade to postgres major version 12.5"

--- infrastructure/database.tf
+++ infrastructure/database.tf
@@ -1,7 +1,6 @@
 # This terraform file hosts the resources directly related to the
 # postgres RDS instance.
 
-### Start Delete After Upgrade to Postgres 12.5
 # Currently exists, will be deleted after aws_db_parameter_group.postgres_parameters is in use.
 resource "aws_db_parameter_group" "postgres_parameters" {
   name = "postgres-parameters-${var.user}-${var.stage}"
@@ -36,40 +35,13 @@ resource "aws_db_parameter_group" "postgres_parameters_new" {
 
   tags = var.default_tags
 }
-### End Delete After Upgrade to Postgres 12.5
-
-resource "aws_db_parameter_group" "postgres12_parameters" {
-  name = "resources-portal-postgres-parameters-${var.user}-${var.stage}"
-  description = "Postgres Parameters ${var.user} ${var.stage}"
-  family = "postgres12"
-
-  parameter {
-    name = "deadlock_timeout"
-    value = "60000" # 60000ms = 60s
-  }
-
-  parameter {
-    name = "statement_timeout"
-    value = "60000" # 60000ms = 60s
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tags = var.default_tags
-}
 
 resource "aws_db_instance" "postgres_db" {
   identifier = "resources-portal-${var.user}-${var.stage}"
   allocated_storage = 100
   storage_type = "gp2"
-  # start temp upgrade options
-  allow_major_version_upgrade = true
-  apply_immediately = true
-  # end temp upgrade options
   engine = "postgres"
-  engine_version = "12.5"
+  engine_version = "9.6.20"
   auto_minor_version_upgrade = false
   instance_class = var.database_instance_type
   name = "resources_portal"
@@ -78,7 +50,7 @@ resource "aws_db_instance" "postgres_db" {
   password = var.database_password
 
   db_subnet_group_name = aws_db_subnet_group.resources_portal.name
-  parameter_group_name = aws_db_parameter_group.postgres12_parameters.name
+  parameter_group_name = aws_db_parameter_group.postgres_parameters_new.name
 
   # TF is broken, but we do want this protection in prod.
   # Related: https://github.com/hashicorp/terraform/issues/5417
