01d7edac9b28c800f32f6bd62077a396e2ef03a6
David Mejia <davidsmejia@gmail.com>
2022-09-07 16:31:33 -0400

    install docker-ce on api instance

--- infrastructure/api-configuration/api-server-instance-user-data.tpl.sh
+++ infrastructure/api-configuration/api-server-instance-user-data.tpl.sh
@@ -17,7 +17,7 @@ cat <<"EOF" > nginx.conf
 ${nginx_config}
 EOF
 apt-get update -y
-apt-get install nginx awscli zip -y
+apt-get install nginx awscli zip docker-ce -y
 cp nginx.conf /etc/nginx/nginx.conf
 service nginx restart
 
