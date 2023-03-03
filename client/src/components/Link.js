bc0cde703e0495a263e9ac2136f2f7f95e92db61
David <davidsmejia@gmail.com>
2023-03-03 15:41:14 -0500

    Revert "Add note to request sign in link"

--- client/src/components/Link.js
+++ client/src/components/Link.js
@@ -2,10 +2,9 @@ import React from 'react'
 import Link from 'next/link'
 import { Anchor } from 'grommet'
 import isExternalPath from 'helpers/isExternalPath'
-import isMailto from 'helpers/isMailto'
 
 export default ({ href, label, icon, as, children = '', color = 'brand' }) => {
-  return isExternalPath(href) || isMailto(href) ? (
+  return isExternalPath(href) ? (
     <Anchor
       target="_blank"
       color={color}
