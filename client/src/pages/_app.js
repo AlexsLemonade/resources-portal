094e4316ad3cce767e3d8827966d459b4231f08e
David Mejia <davidsmejia@gmail.com>
2023-03-06 14:46:28 -0500

    Html and fonts to _document

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -36,14 +36,6 @@ export default ({ Component, pageProps, router: { pathname } }) => {
   return (
     <Sentry.ErrorBoundary fallback={Fallback} showDialog>
       <ResourcesPortalContextProvider>
-        <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,700;1,400&family=Zilla+Slab:wght@500;700&display=swap"
-            rel="stylesheet"
-            key="google-fonts"
-          />
-        </Head>
         <Grommet theme={theme}>
           {isHome && (
             <HomeLayout>
